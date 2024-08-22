90e6d894881dd27f19b4ca6d9fc47a7d3b18019b
HarshMehar50 <harshmehar4955@gmail.com>
2024-08-22 20:22:25 +0530

    Time: 173 ms (16.01%), Space: 55 MB (21.71%) - LeetHub

--- 1748-best-team-with-no-conflicts/1748-best-team-with-no-conflicts.java
+++ 1748-best-team-with-no-conflicts/1748-best-team-with-no-conflicts.java
@@ -1,14 +1,18 @@
 class Solution {
-    int solve(int[][] a , int c , int p){
+    int solve(int[][] a , int c , int p , int[][] dp){
         if(c == a.length){
             return 0;
         }
+        if(dp[c][p+1] != -1){
+            return dp[c][p+1];
+        }
         int include = 0;
         if(p == -1 || (a[c][1] >= a[p][1] && p != -1))
-        include = a[c][1]+solve(a , c+1 , c);
-        int exclude = solve(a , c+1 , p);
+        include = a[c][1]+solve(a , c+1 , c , dp);
+        int exclude = solve(a , c+1 , p , dp);
         int ans = Math.max(include , exclude);
-        return ans;
+        dp[c][p+1] = ans;
+        return dp[c][p+1];
     }
     public int bestTeamScore(int[] scores, int[] ages) {
         int[][] a = new int[ages.length][2];
@@ -16,7 +20,11 @@ class Solution {
             a[i][0] = ages[i];
             a[i][1] = scores[i];
         }
+        int[][] dp = new int[ages.length][ages.length+1];
+        for(int i = 0; i < dp.length; i++){
+            Arrays.fill(dp[i] , -1);
+        }
         Arrays.sort(a,(x , y)->(x[0] != y[0])?Integer.compare(x[0] , y[0]):Integer.compare(x[1] , y[1]));
-        return solve(a , 0 , -1);
+        return solve(a , 0 , -1 , dp);
     }
 }
