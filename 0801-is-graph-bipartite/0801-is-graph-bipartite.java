221f8ba80eb9c555dbe73ca69e1c9b0f4833a5e4
HarshMehar50 <harshmehar4955@gmail.com>
2024-09-08 22:08:42 +0530

    Time: 2 ms (34.28%), Space: 44.8 MB (67.81%) - LeetHub

---
+++ 0801-is-graph-bipartite/0801-is-graph-bipartite.java
@@ -0,0 +1,34 @@
+class Solution {
+    boolean check(int node , HashMap<Integer , int[]> adj , int[] alternate){
+        Queue<Integer> q = new LinkedList<>();
+        q.offer(node);
+        alternate[node] = 0;
+        while(!q.isEmpty()){
+            int cnode = q.poll();
+            for(int i = 0; i < adj.get(cnode).length; i++){
+                if(alternate[adj.get(cnode)[i]] == -1){
+                    alternate[adj.get(cnode)[i]] = 1-alternate[cnode];
+                    q.offer(adj.get(cnode)[i]);
+                }else if(alternate[adj.get(cnode)[i]] == alternate[cnode]){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+    public boolean isBipartite(int[][] graph) {
+        HashMap<Integer , int[]> adj = new HashMap<>();
+        for(int i = 0; i < graph.length; i++){
+            adj.put(i , graph[i]);
+        }
+        int[] alternate = new int[adj.size()];
+        Arrays.fill(alternate , -1);
+        for(int i = 0; i < alternate.length; i++){
+            if(alternate[i] == -1){
+                if(!check(i , adj , alternate))
+                return false;
+            }
+        }
+        return true;
+    }
+}
