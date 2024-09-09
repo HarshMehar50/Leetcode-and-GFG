8e2619c33569d707e195ddc7c1d868c5219af4dd
HarshMehar50 <harshmehar4955@gmail.com>
2024-09-09 15:38:18 +0530

    Time: 55 ms (13%), Space: 58.3 MB (17.81%) - LeetHub

---
+++ 1325-path-with-maximum-probability/1325-path-with-maximum-probability.java
@@ -0,0 +1,39 @@
+class Pair{
+    double probability;
+    int node;
+    public Pair(double probability , int node){
+        this.probability = probability;
+        this.node = node;
+    }
+}
+class Solution {
+    public double maxProbability(int n, int[][] edges, double[] succProb, int start_node, int end_node) {
+        HashMap<Integer , List<Pair>> adj = new HashMap<>();
+        for(int i = 0; i < n; i++){
+            adj.put(i , new ArrayList<>());
+        }
+        for(int i = 0; i < edges.length; i++){
+            adj.get(edges[i][0]).add(new Pair(succProb[i] , edges[i][1]));
+            adj.get(edges[i][1]).add(new Pair(succProb[i] , edges[i][0]));
+        }
+        PriorityQueue<Pair> pq = new PriorityQueue<Pair>((x , y)->Double.compare(y.probability , x.probability));
+        double[] p = new double[n];
+        Arrays.fill(p , Double.MIN_VALUE);
+        p[start_node] = 1;
+        pq.offer(new Pair(1 , start_node));
+        while(pq.size() != 0){
+            double prob = pq.peek().probability;
+            int cnode = pq.peek().node;
+            pq.remove();
+            for(int i = 0; i < adj.get(cnode).size(); i++){
+                double cprob = adj.get(cnode).get(i).probability;
+                int adjnode = adj.get(cnode).get(i).node;
+                if(prob*cprob > p[adjnode]){
+                    p[adjnode] = prob*cprob;
+                    pq.add(new Pair(p[adjnode] , adjnode));
+                }
+            }
+        } 
+        return p[end_node];
+    }
+}
