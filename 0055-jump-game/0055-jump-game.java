e1d9bfd19dad7763b4799a73c515ad47c752bc5b
HarshMehar50 <harshmehar4955@gmail.com>
2024-10-07 13:05:19 +0530

    Time: 2165 ms (5%), Space: 44.8 MB (99.45%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean canJump(int[] nums) {
+        Queue<int[]> q = new LinkedList<>();
+        q.offer(new int[]{0 , nums[0]});
+        boolean[] visited = new boolean[nums.length];
+        visited[0] = true;
+        while(!q.isEmpty()){
+            int index = q.peek()[0];
+            int n = q.peek()[1];
+            q.poll();
+            if(index == nums.length-1)
+            return true;
+            for(int i = 0; i <= nums[index]; i++){
+                if(i+index < nums.length && !visited[i+index]){
+                    q.offer(new int[]{i+index , nums[i+index]});
+                    visited[i+index] = true;
+                }
+            }
+        }
+        return false;
+    }
+}
