b0cc1ccfa580c2d6ec146dcf49ec4b2963016a87
HarshMehar50 <harshmehar4955@gmail.com>
2024-10-22 23:37:57 +0530

    Time: 16 ms (81.82%), Space: 44.9 MB (71.69%) - LeetHub

--- 3558-find-a-safe-walk-through-a-grid/3558-find-a-safe-walk-through-a-grid.java
+++ 3558-find-a-safe-walk-through-a-grid/3558-find-a-safe-walk-through-a-grid.java
@@ -6,7 +6,7 @@ class Solution {
         return false;
         int[][] d = new int[grid.size()][grid.get(0).size()];
         for(int i = 0; i < d.length; i++){
-            Arrays.fill(d[i] , 0);
+            Arrays.fill(d[i] , Integer.MIN_VALUE);
         }
         pq.offer(new int[]{chealth , 0 , 0});
         int[] dR = {1 , 0 , -1 , 0};
@@ -22,7 +22,7 @@ class Solution {
             for(int i = 0; i < 4; i++){
                 int nr = r+dR[i];
                 int nc = c+dC[i];
-                if(nr < grid.size() && nr >= 0 && nc < grid.get(0).size() && nc > 0)
+                if(nr < grid.size() && nr >= 0 && nc < grid.get(0).size() && nc >= 0)
                 if(ch-grid.get(nr).get(nc) > d[nr][nc] && ch-grid.get(nr).get(nc) > 0){
                     d[nr][nc] = ch-grid.get(nr).get(nc);
                     pq.offer(new int[]{ch-grid.get(nr).get(nc) , nr , nc});
