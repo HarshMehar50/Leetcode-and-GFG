d8d82fe2464b2382c18b11ac1d1a3a19a7b9576c
HarshMehar50 <harshmehar4955@gmail.com>
2024-10-24 00:02:05 +0530

    Time: 286 ms (23.28%), Space: 93 MB (17.24%) - LeetHub

--- 3386-find-edges-in-shortest-paths/3386-find-edges-in-shortest-paths.java
+++ 3386-find-edges-in-shortest-paths/3386-find-edges-in-shortest-paths.java
@@ -33,6 +33,7 @@ class Solution {
         int[] dd = dijkstra(n , adj , n-1);
         boolean[] ans = new boolean[edges.length];
         for(int i = 0; i < edges.length; i++){
+            if(ds[edges[i][0]] != Integer.MAX_VALUE && ds[edges[i][1]] != Integer.MAX_VALUE && dd[edges[i][0]] != Integer.MAX_VALUE && dd[edges[i][1]] != Integer.MAX_VALUE)
             if((ds[edges[i][0]]+edges[i][2]+dd[edges[i][1]] == ds[n-1])||(ds[edges[i][1]]+edges[i][2]+dd[edges[i][0]] == ds[n-1]))
             ans[i] = true;
         }
