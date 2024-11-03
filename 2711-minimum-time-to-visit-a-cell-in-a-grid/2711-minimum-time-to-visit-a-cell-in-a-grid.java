f0862f7072797e66af59246226189f240d0bfbdc
HarshMehar50 <harshmehar4955@gmail.com>
2024-11-03 23:39:24 +0530

    Time: 133 ms (71.43%), Space: 60.1 MB (40.76%) - LeetHub

---
+++ 2711-minimum-time-to-visit-a-cell-in-a-grid/2711-minimum-time-to-visit-a-cell-in-a-grid.java
@@ -0,0 +1,39 @@
+class Solution {
+    public int minimumTime(int[][] grid) {
+        PriorityQueue<int[]> pq = new PriorityQueue<int[]>((x , y)->Integer.compare(x[0] , y[0]));
+        boolean[][] visited = new boolean[grid.length][grid[0].length];
+        if(grid[0][1] > 1 && grid[1][0] > 1)
+        return -1;
+        pq.offer(new int[]{grid[0][0] , 0 , 0});
+        int[] dR = {1 , 0 , -1 , 0};
+        int[] dC = {0 , 1 , 0 , -1};
+        visited[0][0] = true;
+        while(!pq.isEmpty()){
+            int time = pq.peek()[0];
+            int r = pq.peek()[1];
+            int c = pq.peek()[2];
+            pq.poll();
+            if(r == grid.length-1 && c == grid[0].length-1)
+            return time;
+            for(int i = 0; i < 4; i++){
+                int nr = r+dR[i];
+                int nc = c+dC[i];
+                if(nr < grid.length && nr >= 0 && nc < grid[0].length && nc >= 0 && !visited[nr][nc]){
+                    if(grid[nr][nc] <= time+1){
+                        pq.offer(new int[]{time+1 , nr , nc});
+                        visited[nr][nc] = true;
+                    }else{
+                        if((grid[nr][nc]-time)%2 == 1){
+                            pq.offer(new int[]{grid[nr][nc] , nr , nc});
+                            visited[nr][nc] = true;
+                        }else{
+                            pq.offer(new int[]{grid[nr][nc]+1 , nr , nc});
+                            visited[nr][nc] = true;
+                        }
+                    }
+                }
+            }
+        }
+        return -1;
+    }
+}
