124b66d8c6901c88855f33d4ecc0af43df2da903
HarshMehar50 <harshmehar4955@gmail.com>
2024-12-05 18:33:47 +0530

    Time: 8 ms (29.17%), Space: 44.8 MB (12.91%) - LeetHub

---
+++ 0909-stone-game/0909-stone-game.java
@@ -0,0 +1,22 @@
+class Solution {
+    int solve(int[] piles , int start , int end){
+        int[][] dp = new int[piles.length][piles.length];
+        for(int i = 0; i < piles.length; i++){
+            dp[i][i] = piles[i];
+        }
+        for(int sal = 2; sal <= piles.length; sal++){
+            for(int i = 0; i <= piles.length-sal; i++){
+                int j = i+sal-1;
+                dp[i][j] = Math.max(piles[i]-dp[i+1][j] , piles[j]-dp[i][j-1]);
+            }
+        }
+        return dp[0][piles.length-1];
+    }
+    public boolean stoneGame(int[] piles) {
+        int ans = solve(piles , 0 , piles.length-1);
+        if(ans > 0)
+        return true;
+        else
+        return false;
+    }
+}
