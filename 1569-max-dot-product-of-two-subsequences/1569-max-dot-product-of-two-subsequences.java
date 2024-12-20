562408c8ec4dcfdb5cfd6de482bcf61cad32969b
HarshMehar50 <harshmehar4955@gmail.com>
2024-12-21 00:55:58 +0530

    Time: 9 ms (85.19%), Space: 44.5 MB (71.3%) - LeetHub

---
+++ 1569-max-dot-product-of-two-subsequences/1569-max-dot-product-of-two-subsequences.java
@@ -0,0 +1,34 @@
+class Solution {
+    /*int solve(int[] nums1 , int[] nums2 , int i , int j){
+        if(i == nums1.length){
+            return 0;
+        }
+        if(j == nums2.length){
+            return 0;
+        }
+        if(i == j && nums1[i]*nums2[j] >= 0){
+            return Math.max(nums1[i]*nums2[j]+solve(nums1 , nums2 , i+1 , j+1) , solve(nums1 , nums2 , i+1 , j+1));
+        }else{
+            return Math.max(solve(nums1, nums2 , i+1 , j) , solve(nums1 , nums2 , i , j+1));
+        }
+    }*/
+    int solve(int[] nums1 , int[] nums2 , int i , int j , int[][] dp){
+        if(i >= nums1.length || j >= nums2.length)
+        return -1000000000;
+        if(dp[i][j] != -1000000000)
+        return dp[i][j];
+        int same = solve(nums1 , nums2 , i+1 , j+1 , dp)+(nums1[i]*nums2[j]);
+        int inc1 = solve(nums1 , nums2 , i+1 , j , dp);
+        int inc2 = solve(nums1 , nums2 , i , j+1 , dp);
+        int ans = Math.max(nums1[i]*nums2[j] , Math.max(same , Math.max(inc1 , inc2)));
+        dp[i][j] = ans;
+        return dp[i][j];
+    }
+    public int maxDotProduct(int[] nums1, int[] nums2) {
+        int[][] dp = new int[nums1.length+1][nums2.length+1];
+        for(int i = 0; i < dp.length; i++){
+            Arrays.fill(dp[i] , -1000000000);
+        }
+        return solve(nums1 , nums2 , 0 , 0 , dp);
+    }
+}
