341b29805b1e520e4eab0ddfd56d370e52fc6976
HarshMehar50 <harshmehar4955@gmail.com>
2025-01-24 10:32:40 +0530

    Time: 1 ms (100%), Space: 40.9 MB (65.75%) - LeetHub

---
+++ 3192-maximum-xor-product/3192-maximum-xor-product.java
@@ -0,0 +1,31 @@
+class Solution {
+    final int mod = 1000000007;
+    public int maximumXorProduct(long a, long b, int n) {
+        long axorx = 0;
+        long bxorx = 0;
+        for(int i = 49; i >= n; i--){
+            long aset = ((a>>i)&1);
+            long bset = ((b>>i)&1);
+            if(aset > 0)
+            axorx ^= (1L<<i);
+            if(bset > 0)
+            bxorx ^= (1L<<i);
+        }
+        for(long i = n-1; i >= 0; i--){
+            long aset = (a&(1L<<i));
+            long bset = (b&(1L<<i));
+            if(aset == bset){
+                axorx ^= (1L<<i);
+                bxorx ^= (1L<<i);
+                continue;
+            }
+            if(axorx > bxorx)
+            bxorx ^= (1L<<i);
+            else
+            axorx ^= (1L<<i);
+        }
+        axorx = axorx%mod;
+        bxorx = bxorx%mod;
+        return (int)((axorx*bxorx)%mod);
+    }
+}
