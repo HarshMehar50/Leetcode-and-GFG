d51d8c1712851cc644b6a4db0702715ba760fcae
HarshMehar50 <harshmehar4955@gmail.com>
2025-01-24 14:41:26 +0530

    Time: 4 ms (83.42%), Space: 45.2 MB (59.02%) - LeetHub

--- 0477-total-hamming-distance/0477-total-hamming-distance.java
+++ 0477-total-hamming-distance/0477-total-hamming-distance.java
@@ -1,11 +1,22 @@
 class Solution {
     public int totalHammingDistance(int[] nums) {
-        int ans = 0;
+        // BRUTE FORCE
+        /*int ans = 0;
         for(int i = 0; i < nums.length-1; i++){
             for(int j = i+1; j < nums.length; j++){
                 int x = nums[i]^nums[j];
                 ans += Integer.bitCount(x);
             }
+        }
+        return ans;*/
+        int ans = 0;
+        for(int i = 0; i < 32; i++){
+            int c1 = 0;
+            for(int j = 0; j < nums.length; j++){
+                c1 += ((nums[j]>>i)&1);
+            }
+            int c0 = nums.length-c1;
+            ans += (c1*c0);
         }
         return ans;
     }
