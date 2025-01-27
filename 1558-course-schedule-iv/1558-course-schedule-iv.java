6e876493d85b62133659659751fea06b4606330e
HarshMehar50 <harshmehar4955@gmail.com>
2025-01-28 03:39:43 +0530

    Time: 400 ms (11.59%), Space: 50.4 MB (13.66%) - LeetHub

---
+++ 1558-course-schedule-iv/1558-course-schedule-iv.java
@@ -0,0 +1,29 @@
+class Solution {
+    boolean DFS(HashMap<Integer , List<Integer>> adj , boolean[] visited , int node , int d){
+        visited[node] = true;
+        if(node == d)
+        return true;
+        boolean ans = false;
+        for(Integer x : adj.get(node)){
+            if(!visited[x])
+            ans = ans||DFS(adj , visited , x , d);
+        }
+        return ans;
+    }
+    public List<Boolean> checkIfPrerequisite(int numCourses, int[][] prerequisites, int[][] queries) {
+        HashMap<Integer , List<Integer>> adj = new HashMap<>();
+        for(int i = 0; i < numCourses; i++){
+            adj.put(i , new ArrayList<>());
+        }
+        for(int i = 0; i < prerequisites.length; i++){
+            adj.get(prerequisites[i][0]).add(prerequisites[i][1]);
+        }
+        List<Boolean> ans = new ArrayList<>();
+        for(int i = 0; i < queries.length; i++){
+            boolean[] visited = new boolean[numCourses];
+            boolean a = DFS(adj , visited , queries[i][0] , queries[i][1]);
+            ans.add(a);
+        }
+        return ans;
+    }
+}
