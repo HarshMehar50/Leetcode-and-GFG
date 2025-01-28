5a60d1d207722669e220efc112b34fac12215b92
HarshMehar50 <harshmehar4955@gmail.com>
2025-01-29 01:03:23 +0530

    Time: 6 ms (5.19%), Space: 42.8 MB (19.37%) - LeetHub

---
+++ 0797-rabbits-in-forest/0797-rabbits-in-forest.java
@@ -0,0 +1,35 @@
+class Solution {
+    public int numRabbits(int[] answers) {
+        /*List<Integer> l = new ArrayList<>();
+        for(int i = 0; i < answers.length; i++){
+            if(!l.contains(answers[i]))
+            l.add(answers[i]);
+        }
+        int s = 0;
+        for(int i = 0; i < l.size(); i++){
+            s += l.get(i);
+        }
+        s += l.size();
+        return s;*/
+        HashMap<Integer , Integer> f = new HashMap<>();
+        for(int i = 0; i < answers.length; i++){
+            f.put(answers[i] , 0);
+        }
+        for(int i = 0; i < answers.length; i++){
+            int v = f.get(answers[i]);
+            f.put(answers[i] , v+1);
+        }
+        int ans = 0;
+        for(int i = 0; i < answers.length; i++){
+            if(answers[i] == 0)
+            ans++;
+            else{
+                if(f.get(answers[i]) > 0)
+                ans += 1+answers[i];
+                int v = f.get(answers[i]);
+                f.put(answers[i] , v-(answers[i]+1));
+            }
+        }
+        return ans;
+    }
+}
