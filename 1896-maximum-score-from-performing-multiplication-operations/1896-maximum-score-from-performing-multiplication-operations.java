ad74a2aa3b7a6b83ed49a52ad802ce8db0a9d7ea
HarshMehar50 <harshmehar4955@gmail.com>
2025-03-23 00:48:40 +0530

    Time: 383 ms (5.14%), Space: 276.9 MB (5.14%) - LeetHub

---
+++ 1896-maximum-score-from-performing-multiplication-operations/1896-maximum-score-from-performing-multiplication-operations.java
@@ -0,0 +1,23 @@
+class Solution {
+    int solve(int[] nums , int[] multipliers , int l , int r , int i , int[][][] dp){
+        if(i >= multipliers.length || l > r)
+        return 0;
+        int r1 = nums.length-1-r;
+        int rf = multipliers.length-1-r1;
+        if(dp[l][rf][i] != -1)
+        return dp[l][rf][i];
+        int ans = 0;
+        ans = Math.max((nums[l]*multipliers[i])+solve(nums , multipliers , l+1 , r , i+1 , dp) , (nums[r]*multipliers[i])+solve(nums , multipliers , l , r-1 , i+1 , dp));
+        dp[l][rf][i] = ans;
+        return dp[l][rf][i];
+    }
+    public int maximumScore(int[] nums, int[] multipliers) {
+        int[][][] dp = new int[multipliers.length][multipliers.length][multipliers.length];
+        for(int[][] a : dp){
+            for(int i = 0; i < a.length; i++){
+                Arrays.fill(a[i] , -1);
+            }
+        }
+        return solve(nums , multipliers , 0 , nums.length-1 , 0 , dp);
+    }
+}
