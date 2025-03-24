d49f716d973a62b520f0207f04d257577df8a8c5
HarshMehar50 <harshmehar4955@gmail.com>
2025-03-24 12:35:58 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 1335-maximum-candies-allocated-to-k-children/1335-maximum-candies-allocated-to-k-children.java
@@ -0,0 +1,32 @@
+class Solution {
+    boolean check(int[] candies , long m , long k){
+        long c = 0;
+        for(int i = 0; i < candies.length; i++){
+            c += candies[i]/m;
+        }
+        if(c >= k)
+        return true;
+        else
+        return false;
+    }
+    public int maximumCandies(int[] candies, long k) {
+        long sum = 0;
+        long max = 0;
+        for(int i = 0; i < candies.length; i++){
+            max = Math.max(max , candies[i]);
+            sum += candies[i];
+        }
+        if(sum < k)
+        return 0;
+        long s = 1;
+        long e = max;
+        while(s <= e){
+            long m = s+(e-s)/2;
+            if(check(candies , m , k))
+            s = m+1;
+            else
+            e = m-1;
+        }
+        return (int)s;
+    }
+}
