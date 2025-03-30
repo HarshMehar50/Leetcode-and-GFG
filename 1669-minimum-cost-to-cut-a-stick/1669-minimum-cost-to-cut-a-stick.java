f28ec72b7aedb6a40683f06935c44a38705173ef
HarshMehar50 <harshmehar4955@gmail.com>
2025-03-31 01:37:24 +0530

    Time: 24 ms (40.69%), Space: 42.3 MB (94.49%) - LeetHub

---
+++ 1669-minimum-cost-to-cut-a-stick/1669-minimum-cost-to-cut-a-stick.java
@@ -0,0 +1,39 @@
+class Solution {
+    int solve(int n , int[] cuts , int l , int r , int[][] dp){
+        if(l > r)
+        return 0;
+        if(dp[l][r] != -1)
+        return dp[l][r];
+        int ans = Integer.MAX_VALUE;
+        for(int i = l; i <= r; i++){
+            int left = 0;
+            int right = 0;
+            if(l-1 < 0)
+            left = 0;
+            else
+            left = cuts[l-1];
+            if(r+1 >= cuts.length)
+            right = n;
+            else
+            right = cuts[r+1];
+            ans = Math.min(ans , right-left+solve(n , cuts , l , i-1 , dp)+solve(n , cuts , i+1 , r , dp));
+        }
+        dp[l][r] = ans;
+        return dp[l][r];
+    }
+    public int minCost(int n, int[] cuts) {
+        /*int[] a = new int[n+2];
+        a[0] = 0;
+        for(int i = 0; i < cuts.length; i++){
+            a[i+1] = cuts[i];
+        }
+        a[n+1] = n;
+        Arrays.sort(a);*/
+        Arrays.sort(cuts);
+        int[][] dp = new int[cuts.length+2][cuts.length+1];
+        for(int i = 0; i < dp.length; i++){
+            Arrays.fill(dp[i] , -1);
+        }
+        return solve(n , cuts , 0 , cuts.length-1 , dp);
+    }
+}
