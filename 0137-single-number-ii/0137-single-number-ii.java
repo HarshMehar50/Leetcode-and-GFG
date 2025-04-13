11b9a7a05e36cf342bcdcaa60582b2c4ce4fb18c
HarshMehar50 <harshmehar4955@gmail.com>
2025-04-13 09:58:02 +0530

    Time: 7 ms (9.06%), Space: 46.3 MB (7%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.java
+++ 0137-single-number-ii/0137-single-number-ii.java
@@ -11,8 +11,8 @@ class Solution {
         int ans = 0;
         for(int i = 0; i < nums.length; i++){
             if(map.get(nums[i]) == 1){
-            ans = nums[i];
-            break;
+                ans = nums[i];
+                break;
             }
         }
         return ans;
