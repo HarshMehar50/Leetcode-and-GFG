b8e8a1fb3e80f1366252ce2b08d6f9a56d6d7895
HarshMehar50 <harshmehar4955@gmail.com>
2025-04-15 15:14:01 +0530

    Time: 2 ms (14.46%), Space: 43.7 MB (64.34%) - LeetHub

--- 0655-print-binary-tree/0655-print-binary-tree.java
+++ 0655-print-binary-tree/0655-print-binary-tree.java
@@ -15,19 +15,19 @@
  */
 class Solution {
     class Node{
-        TreeNode node;
+    TreeNode node;
         int r;
         int c;
         public Node(TreeNode node , int r , int c){
             this.node = node;
             this.r = r;
             this.c = c;
-        } 
+        }
     }
     int height(TreeNode root){
         List<List<Integer>> list = new ArrayList<>();
         if(root == null)
-        return 0;
+            return 0;
         Queue<TreeNode> q = new LinkedList<>();
         q.offer(root);
         while(!q.isEmpty()){
@@ -37,11 +37,11 @@ class Solution {
                 TreeNode node = q.poll();
                 inner.add(node.val);
                 if(node.left != null){
-                q.offer(node.left);
+                    q.offer(node.left);
                 }
                 if(node.right != null){
-                q.offer(node.right);
-                } 
+                    q.offer(node.right);
+                }
             }
             list.add(inner);
         }
@@ -51,7 +51,7 @@ class Solution {
         int h = height(root);
         List<List<String>> ans = new ArrayList<>();
         if(root == null)
-        return ans;
+            return ans;
         for(int i = 0; i <= h; i++){
             List<String> inner = new ArrayList<>((int)(Math.pow(2 , h+1)-1));
             for(int j = 0; j < (int)(Math.pow(2 , h+1)-1); j++){
@@ -70,10 +70,10 @@ class Solution {
                 q.poll();
                 ans.get(r).set(c , Integer.toString(node.val));
                 if(node.left != null){
-                q.offer(new Node(node.left , r+1 , (int)(c-Math.pow(2 , h-r-1))));
+                    q.offer(new Node(node.left , r+1 , (int)(c-Math.pow(2 , h-r-1))));
                 }
                 if(node.right != null){
-                q.offer(new Node(node.right , r+1 , (int)(c+Math.pow(2 , h-r-1))));
+                    q.offer(new Node(node.right , r+1 , (int)(c+Math.pow(2 , h-r-1))));
                 }
             }
         }
