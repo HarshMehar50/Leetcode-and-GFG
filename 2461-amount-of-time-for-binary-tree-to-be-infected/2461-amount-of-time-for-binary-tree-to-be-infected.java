2003dfd64af89e6d9bfa7768cad860d73a578933
HarshMehar50 <harshmehar4955@gmail.com>
2025-04-17 11:52:14 +0530

    Time: 171 ms (7.9%), Space: 118.3 MB (14.14%) - LeetHub

--- 2461-amount-of-time-for-binary-tree-to-be-infected/2461-amount-of-time-for-binary-tree-to-be-infected.java
+++ 2461-amount-of-time-for-binary-tree-to-be-infected/2461-amount-of-time-for-binary-tree-to-be-infected.java
@@ -14,7 +14,7 @@
  * }
  */
 class Solution {
-     static void createParentList(TreeNode root , List<Integer> parent){
+    static void createParentList(TreeNode root , List<Integer> parent){
         if(root == null){
             return;
         }
@@ -22,13 +22,13 @@ class Solution {
         createParentList(root.left , parent);
         createParentList(root.right , parent);
     }
-   static void createAdjList(List<Integer> parent , TreeNode root , HashMap<Integer , List<Integer>> adj){
+    static void createAdjList(List<Integer> parent , TreeNode root , HashMap<Integer , List<Integer>> adj){
        /* for(int i = 0; i < parent.size(); i++){
             adj.put(parent.get(i) , new ArrayList<>());
         }*/
-       if(root == null){
-           return;
-       }
+        if(root == null){
+            return;
+        }
         if(root.left != null) {
             adj.get(root.val).add(root.left.val);
             adj.get(root.left.val).add(root.val);
@@ -38,9 +38,9 @@ class Solution {
             adj.get(root.right.val).add(root.val);
         }
         createAdjList(parent , root.left , adj);
-       createAdjList(parent , root.right , adj);
-   }
-   static List<List<Integer>> BFSLevel(HashMap<Integer , List<Integer>> adj , List<Integer> parent , int s){
+        createAdjList(parent , root.right , adj);
+    }
+    static List<List<Integer>> BFSLevel(HashMap<Integer , List<Integer>> adj , List<Integer> parent , int s){
         List<List<Integer>> list = new ArrayList<>();
         HashMap<Integer , Boolean> visited = new HashMap<>();
         for(int i = 0; i < parent.size(); i++){
@@ -67,9 +67,9 @@ class Solution {
         return list;
     }
     public int amountOfTime(TreeNode root, int start) {
-         List<Integer> parent = new ArrayList<>();
+        List<Integer> parent = new ArrayList<>();
         createParentList(root , parent);
-         HashMap<Integer , List<Integer>> adj = new HashMap<>();
+        HashMap<Integer , List<Integer>> adj = new HashMap<>();
         for(int i = 0; i < parent.size(); i++){
             adj.put(parent.get(i) , new ArrayList<>());
         }
