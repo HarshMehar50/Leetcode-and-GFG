acf5db1a79bc3aa5806e1da0585207b82bfe0665
HarshMehar50 <harshmehar4955@gmail.com>
2025-05-20 17:11:04 +0530

    Time: 7 ms (11.25%), Space: 41.8 MB (14.11%) - LeetHub

---
+++ 0052-n-queens-ii/0052-n-queens-ii.java
@@ -0,0 +1,49 @@
+class Solution {
+    boolean check(int r , int c , List<String> inner , int n){
+        for(int i = r; i >= 0; i--){
+            if(inner.get(i).charAt(c) == 'Q')
+            return false;
+        }
+        for(int i = r , j = c; i >= 0 && j >= 0; i-- , j--){
+            if(inner.get(i).charAt(j) == 'Q')
+            return false;
+        }
+        for(int i = r , j = c; i >= 0 && j < n; i-- , j++){
+            if(inner.get(i).charAt(j) == 'Q')
+            return false;
+        }
+        return true;
+    }
+    void solve(int n , List<List<String>> ans , List<String> inner , int r){
+        if(r == n){
+            ans.add(new ArrayList<>(inner));
+            return;
+        }
+        for(int i = 0; i < n; i++){
+            if(check(r , i , inner , n)){
+                String s = inner.get(r);
+                /*String t = s;
+                s = s.substring(0 , i)+'Q'+s.substring(i+1);
+                inner.set(r , s);*/
+                StringBuilder sb = new StringBuilder(s);
+                sb.setCharAt(i , 'Q');
+                inner.set(r , sb.toString());
+                solve(n , ans , inner , r+1);
+                inner.set(r , s);
+            }
+        }
+    }
+    public int totalNQueens(int n) {
+        List<List<String>> ans = new ArrayList<>();
+        String s = "";
+        for(int i = 0; i < n; i++){
+            s += '.';
+        }
+        List<String> inner = new ArrayList<>();
+        for(int i = 0; i < n; i++){
+            inner.add(s);
+        }
+        solve(n , ans , inner , 0);
+        return ans.size();
+    }
+}
