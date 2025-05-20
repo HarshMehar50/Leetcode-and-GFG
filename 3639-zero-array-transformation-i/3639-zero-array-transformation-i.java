715323134ee125e4d26e32bbcd2cd29882778f5f
HarshMehar50 <harshmehar4955@gmail.com>
2025-05-20 11:12:45 +0530

    Time: 3 ms (79.52%), Space: 95.9 MB (30.87%) - LeetHub

--- 3639-zero-array-transformation-i/3639-zero-array-transformation-i.java
+++ 3639-zero-array-transformation-i/3639-zero-array-transformation-i.java
@@ -1,26 +1,19 @@
 class Solution {
     public boolean isZeroArray(int[] nums, int[][] queries) {
-        /*int[] d = new int[nums.length];
+        int[] d = new int[nums.length];
         for(int i = 0; i < queries.length; i++){
-            for(int j = queries[i][0]; j <= queries[i][1]; j++){
-                d[j]++;
-            }
+            d[queries[i][0]]--;
+            if(queries[i][1] != nums.length-1)
+            d[queries[i][1]+1]++;
+        }
+        for(int i = 1; i < d.length; i++){
+            d[i] += d[i-1];
         }
         for(int i = 0; i < nums.length; i++){
-            if(d[i] < nums[i])
-            return false;
+            nums[i] += d[i];
         }
-        return true;*/
-        int[] d = new int[nums.length+1];
-        for(int i = 0; i < queries.length; i++){
-            d[queries[i][0]]++;
-            if(queries[i][1]+1 < nums.length)
-            d[queries[i][1]+1]--;
-        }
-        int c = 0;
         for(int i = 0; i < nums.length; i++){
-            c += d[i];
-            if(c < nums[i])
+            if(nums[i] > 0)
             return false;
         }
         return true;
