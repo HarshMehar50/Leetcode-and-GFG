37304c409c35e0e06cdf2438561f1a1c257cfb4b
HarshMehar50 <harshmehar4955@gmail.com>
2025-05-22 00:21:09 +0530

    Time: 17 ms (64.65%), Space: 45.1 MB (64.33%) - LeetHub

--- 3558-find-a-safe-walk-through-a-grid/3558-find-a-safe-walk-through-a-grid.java
+++ 3558-find-a-safe-walk-through-a-grid/3558-find-a-safe-walk-through-a-grid.java
@@ -3,7 +3,7 @@ class Solution {
         PriorityQueue<int[]> pq = new PriorityQueue<int[]>((x , y)->Integer.compare(y[0] , x[0]));
         int chealth = health-grid.get(0).get(0);
         if(chealth <= 0)
-        return false;
+            return false;
         int[][] d = new int[grid.size()][grid.get(0).size()];
         for(int i = 0; i < d.length; i++){
             Arrays.fill(d[i] , Integer.MIN_VALUE);
@@ -18,15 +18,15 @@ class Solution {
             int c = pq.peek()[2];
             pq.poll();
             if(r == grid.size()-1 && c == grid.get(0).size()-1 && ch >= 1)
-            return true;
+                return true;
             for(int i = 0; i < 4; i++){
                 int nr = r+dR[i];
                 int nc = c+dC[i];
                 if(nr < grid.size() && nr >= 0 && nc < grid.get(0).size() && nc >= 0)
-                if(ch-grid.get(nr).get(nc) > d[nr][nc] && ch-grid.get(nr).get(nc) > 0){
-                    d[nr][nc] = ch-grid.get(nr).get(nc);
-                    pq.offer(new int[]{ch-grid.get(nr).get(nc) , nr , nc});
-                }
+                    if(ch-grid.get(nr).get(nc) > d[nr][nc] && ch-grid.get(nr).get(nc) > 0){
+                        d[nr][nc] = ch-grid.get(nr).get(nc);
+                        pq.offer(new int[]{ch-grid.get(nr).get(nc) , nr , nc});
+                    }
             }
         }
         return false;
