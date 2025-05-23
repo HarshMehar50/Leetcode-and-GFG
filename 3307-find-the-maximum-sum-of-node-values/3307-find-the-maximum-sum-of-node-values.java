1b5b9c259edc9285068ec680ca276b9f7405792a
HarshMehar50 <harshmehar4955@gmail.com>
2025-05-23 15:11:38 +0530

    Time: 2 ms (76.32%), Space: 54.9 MB (52.63%) - LeetHub

---
+++ 3307-find-the-maximum-sum-of-node-values/3307-find-the-maximum-sum-of-node-values.java
@@ -0,0 +1,18 @@
+class Solution {
+    public long maximumValueSum(int[] nums, int k, int[][] edges) {
+        long ans = 0;
+        int c = 0;
+        int minxor = Integer.MAX_VALUE;
+        for(int i = 0; i < nums.length; i++){
+            if((nums[i]^k) > nums[i]){
+                ans += nums[i]^k;
+                c++;
+            }else
+            ans += nums[i];
+            minxor = Math.min(minxor , Math.abs(nums[i]-(nums[i]^k)));
+        }
+        if(c%2 == 1)
+        ans = ans-minxor;
+        return ans;
+    }
+}
