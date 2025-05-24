0f8193e4a0cd2a3f5aefb0e7976aedcdb14f183d
HarshMehar50 <harshmehar4955@gmail.com>
2025-05-24 23:48:06 +0530

    Time: 3 ms (68.02%), Space: 43 MB (14.33%) - LeetHub

--- 0097-interleaving-string/0097-interleaving-string.java
+++ 0097-interleaving-string/0097-interleaving-string.java
@@ -1,16 +1,16 @@
 class Solution {
     boolean solve(String s1 , String s2 , String s3 , int i , int j , HashMap<Pair<Integer , Integer> , Boolean> dp){
         if(i == s1.length() && j == s2.length() && i+j == s3.length())
-        return true;
+            return true;
         if(i+j >= s3.length())
-        return false;
+            return false;
         if(dp.containsKey(new Pair<>(i , j)))
-        return dp.get(new Pair<>(i , j));
+            return dp.get(new Pair<>(i , j));
         boolean ans = false;
         if(i < s1.length() && i+j < s3.length() && s1.charAt(i) == s3.charAt(i+j))
-        ans = ans||solve(s1 , s2 , s3 , i+1 , j , dp);
+            ans = ans||solve(s1 , s2 , s3 , i+1 , j , dp);
         if(j < s2.length() && i+j < s3.length() && s2.charAt(j) == s3.charAt(i+j))
-        ans = ans||solve(s1 , s2 , s3 , i , j+1 , dp);
+            ans = ans||solve(s1 , s2 , s3 , i , j+1 , dp);
         dp.put(new Pair<>(i , j) , ans);
         return dp.get(new Pair<>(i , j));
     }
