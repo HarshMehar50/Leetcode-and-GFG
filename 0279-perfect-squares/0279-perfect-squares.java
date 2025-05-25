07c88688dec95879898d141c6baf51478f04a100
HarshMehar50 <harshmehar4955@gmail.com>
2025-05-26 01:48:05 +0530

    Time: 47 ms (41.77%), Space: 42.5 MB (89.5%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.java
+++ 0279-perfect-squares/0279-perfect-squares.java
@@ -1,5 +1,5 @@
 class Solution {
-   /* List<Integer> createList(int n){
+    /* List<Integer> createList(int n){
         List<Integer> l = new ArrayList<>();
         for(int i = 1; i <= n/2; i++){
             l.add(i*i);
@@ -7,20 +7,18 @@ class Solution {
         return l;
     }*/
     public int numSquares(int n) {
-       int[] dp = new int[n+1];
-       for(int i = 0; i < n+1; i++){
-        dp[i] = Integer.MAX_VALUE;
-       }
-       dp[0] = 0;
-       for(int i = 1; i < n+1; i++){
-        for(int j = 1; j*j <= n; j++){
-            if((i-j*j)>=0){
-            dp[i] = Math.min(dp[i-j*j]+1 , dp[i]);
+        int[] dp = new int[n+1];
+        for(int i = 0; i < n+1; i++){
+            dp[i] = Integer.MAX_VALUE;
+        }
+        dp[0] = 0;
+        for(int i = 1; i < n+1; i++){
+            for(int j = 1; j*j <= n; j++){
+                if((i-j*j)>=0){
+                    dp[i] = Math.min(dp[i-j*j]+1 , dp[i]);
+                }
             }
         }
-       }
-       
-       return dp[n];
-       
+        return dp[n];
     }
 }
