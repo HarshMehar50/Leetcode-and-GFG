aab96dcfebdf4d1bb400a74e4edf81f1e4db79e5
HarshMehar50 <harshmehar4955@gmail.com>
2025-05-28 23:57:01 +0530

    Time: 7 ms (5.74%), Space: 40.3 MB (84.93%) - LeetHub

---
+++ 0397-integer-replacement/0397-integer-replacement.java
@@ -0,0 +1,45 @@
+class Solution {
+    int solve(long n){
+        if(n == 1){
+            return 0;
+        }
+        if(n < 1){
+            return Integer.MAX_VALUE;
+        }
+        /*if(dp.containsKey(n)){
+            return dp.get(n);
+        }*/
+        int ans = 0;
+        int includeOddAdd = 0;
+        int includeOddSub = 0;
+        if(n%2 == 0){
+            ans = 1+solve(n/2);
+        }else{
+            includeOddAdd = solve(n+1);
+            includeOddSub = solve(n-1);
+            ans = 1+Math.min(includeOddSub , includeOddAdd);
+        }
+        //dp.put(n , ans);
+        return ans;
+    }
+    /*int solveTab(int n){
+        int[] dp = new int[n+1];
+        dp[1] = 0;
+        for(int i = 2; i < n; i++){
+             int includeEven = 0;
+             int oddans = 0;
+             if(i%2 == 0){
+               includeEven = 1+dp[i/2];
+            }else{
+                oddans = 1+Math.min(dp[i+1] , dp[i-1]);
+            }
+        int ans = includeEven + oddans;
+        dp[i] = ans;
+        }
+        return dp[n];
+    }*/
+    public int integerReplacement(int n) {
+        return solve((long)n);
+        //return solve(n);
+    }
+}
