d264779fa0cc53473b1eb43774bd7850d56e1919
HarshMehar50 <harshmehar4955@gmail.com>
2025-05-30 00:25:23 +0530

    Time: 59 ms (57.26%), Space: 45.3 MB (88.71%) - LeetHub

--- 2198-process-restricted-friend-requests/2198-process-restricted-friend-requests.java
+++ 2198-process-restricted-friend-requests/2198-process-restricted-friend-requests.java
@@ -22,7 +22,7 @@ class Solution {
             int up = findParent(u);
             int vp = findParent(v);
             if(up == vp)
-            return;
+                return;
             if(rank[up] < rank[vp]){
                 parent[up] = vp;
             }else if(rank[vp] < rank[up]){
@@ -36,7 +36,7 @@ class Solution {
             int up = findParent(u);
             int vp = findParent(v);
             if(up == vp)
-            return;
+                return;
             if(size[up] < size[vp]){
                 parent[up] = vp;
                 size[vp] = size[vp]+size[up];
@@ -67,7 +67,7 @@ class Solution {
                 }
             }
             if(ans[i])
-            ds.unionSetRank(requests[i][0] , requests[i][1]);
+                ds.unionSetRank(requests[i][0] , requests[i][1]);
         }
         return ans;
     }
