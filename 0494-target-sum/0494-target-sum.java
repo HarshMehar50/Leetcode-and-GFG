89ffcd1b9675aad742abc4cf3c4def82dad3b91f
HarshMehar50 <harshmehar4955@gmail.com>
2025-05-30 00:23:54 +0530

    Time: 603 ms (27.05%), Space: 41.5 MB (69.91%) - LeetHub

--- 0494-target-sum/0494-target-sum.java
+++ 0494-target-sum/0494-target-sum.java
@@ -2,11 +2,11 @@ class Solution {
     int solve(int[] nums, int target , int i , int s , int[][] dp){
         if(i == nums.length){
             if(s == target)
-            return 1;
+                return 1;
             else return 0;
         }
         if(dp[i][s+1000] != -1)
-        return dp[i][s+1000];
+            return dp[i][s+1000];
         int p = solve(nums , target , i+1 , s+nums[i] , dp);
         int m = solve(nums , target , i+1 , s-nums[i] , dp);
         int ans = p+m;
@@ -16,7 +16,7 @@ class Solution {
     int solveRec(int[] nums, int target , int i , int s){
         if(i == nums.length){
             if(s == target)
-            return 1;
+                return 1;
             else return 0;
         }
         int p = solveRec(nums , target , i+1 , s+nums[i]);
@@ -25,11 +25,11 @@ class Solution {
         return ans;
     }
     public int findTargetSumWays(int[] nums, int target) {
-        int[][] dp = new int[nums.length][2001];
+        /*int[][] dp = new int[nums.length][2001];
         for(int i = 0; i < dp.length; i++){
             Arrays.fill(dp[i] , -1);
         }
-        return solve(nums , target , 0 , 0 , dp);
-       // return solveRec(nums , target , 0 , 0);
+        return solve(nums , target , 0 , 0 , dp);*/
+        return solveRec(nums , target , 0 , 0);
     }
 }
