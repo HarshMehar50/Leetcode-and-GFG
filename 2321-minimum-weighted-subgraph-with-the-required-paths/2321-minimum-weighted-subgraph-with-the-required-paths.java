cb977afa309ba38617a3ffa6473895cb3a3da6be
HarshMehar50 <harshmehar4955@gmail.com>
2025-05-31 00:27:05 +0530

    Time: 269 ms (9.26%), Space: 110.8 MB (70.18%) - LeetHub

--- 2321-minimum-weighted-subgraph-with-the-required-paths/2321-minimum-weighted-subgraph-with-the-required-paths.java
+++ 2321-minimum-weighted-subgraph-with-the-required-paths/2321-minimum-weighted-subgraph-with-the-required-paths.java
@@ -18,7 +18,7 @@ class Solution {
             long distance = pq.peek().distance;
             pq.poll();
             if(distance != d[node])
-            continue;
+                continue;
             for(int i = 0; i < adj.get(node).size(); i++){
                 long weight = adj.get(node).get(i).distance;
                 int adjnode = adj.get(node).get(i).node;
@@ -45,15 +45,42 @@ class Solution {
         long[] ds2 = dijkstra(n , adj , src2);
         long[] dd = dijkstra(n , adjr , dest);
         if(ds1[dest] == Long.MAX_VALUE || ds2[dest] == Long.MAX_VALUE)
-        return -1;
+            return -1;
         long ans = Long.MAX_VALUE;
         for(int i = 0; i < n; i++){
             if(ds1[i] != Long.MAX_VALUE && ds2[i] != Long.MAX_VALUE && dd[i] != Long.MAX_VALUE)
-            ans = Math.min(ans , ds1[i]+ds2[i]+dd[i]);
+                ans = Math.min(ans , ds1[i]+ds2[i]+dd[i]);
         }
         if(ans != Long.MAX_VALUE)
-        return ans;
+            return ans;
         else
-        return -1;
+            return -1;
+    }
+
+    // 2290
+    public int minimumObstacles(int[][] grid) {
+        PriorityQueue<int[]> pq = new PriorityQueue<int[]>((x , y)->Integer.compare(x[0] , y[0]));
+        int[][] d = new int[grid.length][grid[0].length];
+        for(int i = 0; i < grid.length; i++){
+            Arrays.fill(d[i] , Integer.MAX_VALUE);
+        }
+        int[] dR = {1 , 0 , -1 , 0};
+        int[] dC = {0 , 1 , 0 , -1};
+        pq.offer(new int[]{0 , 0 , 0});
+        while(!pq.isEmpty()){
+            int distance = pq.peek()[0];
+            int r = pq.peek()[1];
+            int c = pq.peek()[2];
+            pq.poll();
+            for(int i = 0; i < 4; i++){
+                int nr = r+dR[i];
+                int nc = c+dC[i];
+                if(nr < grid.length && nr >= 0 && nc < grid[0].length && nc >= 0 && distance+grid[nr][nc] < d[nr][nc]){
+                    d[nr][nc] = distance+grid[nr][nc];
+                    pq.offer(new int[]{d[nr][nc] , nr , nc});
+                }
+            }
+        }
+        return d[grid.length-1][grid[0].length-1];
     }
 }
