7109eb7bd8e9d455cc8c34520024aad39dfea985
HarshMehar50 <harshmehar4955@gmail.com>
2025-06-01 00:51:25 +0530

    Time: 20 ms (26.39%), Space: 45.6 MB (13.32%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.java
+++ 0647-palindromic-substrings/0647-palindromic-substrings.java
@@ -5,18 +5,18 @@ class Solution {
             for(int i = 0; i+l-1 < s.length(); i++){
                 int j = i+l-1;
                 if(i == j)
-                dp[i][j] = true;
+                    dp[i][j] = true;
                 else if(i+1 < s.length() && i+1 == j && s.charAt(i) == s.charAt(j))
-                dp[i][j] = true;
+                    dp[i][j] = true;
                 else
-                dp[i][j] = dp[i+1][j-1]&&(s.charAt(i) == s.charAt(j));
+                    dp[i][j] = dp[i+1][j-1]&&(s.charAt(i) == s.charAt(j));
             }
         }
         int ans = 0;
         for(int i = 0; i < s.length(); i++){
             for(int j = 0; j < s.length(); j++){
                 if(dp[i][j])
-                ans++;
+                    ans++;
             }
         }
         return ans;
