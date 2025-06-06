fb9e3cdee43dadfe9eb7ba3b7c3eb38c131525de
HarshMehar50 <harshmehar4955@gmail.com>
2025-06-07 00:47:17 +0530

    Time: 9 ms (52.21%), Space: 45.5 MB (23.31%) - LeetHub

--- 0329-longest-increasing-path-in-a-matrix/0329-longest-increasing-path-in-a-matrix.java
+++ 0329-longest-increasing-path-in-a-matrix/0329-longest-increasing-path-in-a-matrix.java
@@ -3,15 +3,15 @@ class Solution {
     int[] dC = {0 , 1 , 0 , -1};
     int solve(int[][] matrix , int i , int j , int[][] dp){
         if(i < 0 || j < 0 || i >= matrix.length || j >= matrix[0].length)
-        return 0;
+            return 0;
         if(dp[i][j] != -1)
-        return dp[i][j];
+            return dp[i][j];
         int ans = 0;
         for(int k = 0; k < 4; k++){
             int nr = i+dR[k];
             int nc = j+dC[k];
             if(nr < matrix.length && nr >= 0 && nc < matrix[0].length && nc >= 0 && matrix[nr][nc] > matrix[i][j])
-            ans = Math.max(ans , 1+solve(matrix , nr , nc , dp));
+                ans = Math.max(ans , 1+solve(matrix , nr , nc , dp));
         }
         dp[i][j] = ans;
         return dp[i][j];
@@ -25,9 +25,9 @@ class Solution {
         for(int i = 0; i < matrix.length; i++){
             for(int j = 0; j < matrix[0].length; j++){
                 if(dp[i][j] != -1)
-                ans = Math.max(ans , 1+dp[i][j]);
+                    ans = Math.max(ans , 1+dp[i][j]);
                 else
-                ans = Math.max(ans , 1+solve(matrix , i , j , dp));
+                    ans = Math.max(ans , 1+solve(matrix , i , j , dp));
             }
         }
         return ans;
