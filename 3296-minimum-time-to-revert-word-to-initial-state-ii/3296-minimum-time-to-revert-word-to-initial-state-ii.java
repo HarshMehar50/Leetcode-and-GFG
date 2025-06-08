0207cd73c6c44cad9f7bec1daaf67052095baf4c
HarshMehar50 <harshmehar4955@gmail.com>
2025-06-08 12:58:08 +0530

    Time: 346 ms (5%), Space: 124.4 MB (7.5%) - LeetHub

---
+++ 3296-minimum-time-to-revert-word-to-initial-state-ii/3296-minimum-time-to-revert-word-to-initial-state-ii.java
@@ -0,0 +1,105 @@
+class Solution {
+    class Hashing {
+        String s;
+        int n;
+        int primesCount;
+        List<Long> hashPrimes = Arrays.asList(1000000009L, 1000000007L);
+        final long base = 27;
+        List<long[]> hashValues;
+        List<long[]> powersOfBase;
+        List<long[]> inversePowersOfBase;
+        public Hashing(String a) {
+            s = a;
+            n = s.length();
+            primesCount = hashPrimes.size();
+            hashValues = new ArrayList<>();
+            powersOfBase = new ArrayList<>();
+            inversePowersOfBase = new ArrayList<>();
+            for (int i = 0; i < primesCount; i++) {
+                long[] power = new long[n + 1];
+                long[] invPower = new long[n + 1];
+                long mod = hashPrimes.get(i);
+                power[0] = 1;
+                for (int j = 1; j <= n; j++) {
+                    power[j] = (power[j - 1] * base) % mod;
+                }
+                invPower[n] = modInverse(power[n], mod);
+                for (int j = n - 1; j >= 0; j--) {
+                    invPower[j] = (invPower[j + 1] * base) % mod;
+                }
+                powersOfBase.add(power);
+                inversePowersOfBase.add(invPower);
+            }
+            for (int i = 0; i < primesCount; i++) {
+                long[] hash = new long[n];
+                long mod = hashPrimes.get(i);
+                long[] power = powersOfBase.get(i);
+                for (int j = 0; j < n; j++) {
+                    hash[j] = ((s.charAt(j) - 'a' + 1L) * power[j]) % mod;
+                    if (j > 0) hash[j] = (hash[j] + hash[j - 1]) % mod;
+                }
+                hashValues.add(hash);
+            }
+        }
+        public List<Long> substringHash(int l, int r) {
+            List<Long> hash = new ArrayList<>();
+            for (int i = 0; i < primesCount; i++) {
+                long mod = hashPrimes.get(i);
+                long[] hashVal = hashValues.get(i);
+                long[] invPower = inversePowersOfBase.get(i);
+                long val1 = hashVal[r];
+                long val2 = l > 0 ? hashVal[l - 1] : 0;
+                long subHash = (val1 - val2 + mod) % mod;
+                subHash = (subHash * invPower[l]) % mod;
+                hash.add(subHash);
+            }
+            return hash;
+        }
+        private long modInverse(long a, long mod) {
+            return powMod(a, mod - 2, mod);
+        }
+        private long powMod(long a, long b, long mod) {
+            long result = 1;
+            a %= mod;
+            while (b > 0) {
+                if ((b & 1) == 1) 
+                result = (result * a) % mod;
+                a = (a * a) % mod;
+                b >>= 1;
+            }
+            return result;
+        }
+    }
+    public int minimumTimeToInitialState(String word, int k) {
+        if(k == word.length())
+        return 1;
+        Hashing h = new Hashing(word);
+        /*int l = 0;
+        for(int i = 0; i < word.length()-1; i++){
+            List<Long> l1 = h.substringHash(0 , i);
+            List<Long> l2 = h.substringHash(word.length()-i-1 , word.length()-1);
+            if(l1.equals(l2))
+            l = Math.max(l , i+1);
+        }
+        int max = word.length()/k;
+        if(word.length()%k != 0)
+        max++;
+        int r = l/k;
+        if(l%k != 0)
+        r++;
+        int ans = max-r;
+        return ans;*/
+        for(int i = 1; i*k < word.length(); i++){
+            int s = i*k;
+            int l = word.length()-s;
+            List<Long> l1 = h.substringHash(0 , l-1);
+            List<Long> l2 = h.substringHash(s , word.length()-1);
+            if(l1.equals(l2))
+            return i;
+        }
+        int ans = word.length()/k;
+        if(word.length()%k != 0)
+        ans++;
+        return ans;
+    }
+}
