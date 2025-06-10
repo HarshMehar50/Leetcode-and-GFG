98551ab6dac19c6b100866970be5e9722228c2c5
HarshMehar50 <harshmehar4955@gmail.com>
2025-06-11 00:59:32 +0530

    Time: 25 ms (19.05%), Space: 44.7 MB (84.84%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.java
+++ 0044-wildcard-matching/0044-wildcard-matching.java
@@ -1,5 +1,4 @@
 class Solution {
-    // tabulation method is based and possible on 1 based indexing
     boolean solveTab(String s , String p){
         boolean[][] dp = new boolean[s.length()+1][p.length()+1];
         dp[0][0] = true;
@@ -15,17 +14,17 @@ class Solution {
         }
         for(int i = 1; i <= s.length(); i++){
             for(int j = 1; j <= p.length(); j++){
-                if(s.charAt(i-1) == p.charAt(j-1))
-                dp[i][j] = dp[i-1][j-1];
+                if(s.charAt(i-1) == p.charAt(j-1) || p.charAt(j-1) == '?')
+                    dp[i][j] = dp[i-1][j-1];
                 else if(p.charAt(j-1) == '*')
-                dp[i][j] = dp[i][j-1] || dp[i-1][j];
+                    dp[i][j] = dp[i][j-1] || dp[i-1][j];
                 else
-                dp[i][j] = false;
+                    dp[i][j] = false;
             }
         }
         return dp[s.length()][p.length()];
     }
-    boolean solve(String s , String p , int i , int j){
+    boolean solve(String s , String p , int i , int j , int[][] dp){
         if(i < 0 && j < 0){
             return true;
         }
@@ -40,16 +39,27 @@ class Solution {
             }
             return true;
         }
+        if(dp[i][j] != -1){
+            if(dp[i][j] == 1)
+            return true;
+            else
+            return false;
+        }
         if(s.charAt(i) == p.charAt(j) || p.charAt(j) == '?'){
-            return solve(s , p , i-1 , j-1);
+            return solve(s , p , i-1 , j-1 , dp);
         }else if(p.charAt(j) == '*'){
-            return solve(s , p , i-1 , j)||solve(s , p , i , j-1);
+            return solve(s , p , i-1 , j , dp)||solve(s , p , i , j-1 , dp);
         }else{
             return false;
         }
     }
     public boolean isMatch(String s, String p) {
-        //return solve(s , p , s.length()-1 , p.length()-1);
+        /*int[][] dp = new int[s.length()][p.length()];
+        for(int i = 0; i < dp.length; i++){
+            Arrays.fill(dp[i] , -1);
+        }
+        boolean ans = solve(s , p , s.length()-1 , p.length()-1 , dp);
+        return ans;*/
         return solveTab(s , p);
     }
 }
