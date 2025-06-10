dc781ceef091d9e66924fe38062c52c10960cfd6
HarshMehar50 <harshmehar4955@gmail.com>
2025-06-11 00:36:19 +0530

    Time: 9 ms (27.39%), Space: 44.7 MB (31.21%) - LeetHub

--- 1251-longest-chunked-palindrome-decomposition/1251-longest-chunked-palindrome-decomposition.java
+++ 1251-longest-chunked-palindrome-decomposition/1251-longest-chunked-palindrome-decomposition.java
@@ -1,5 +1,4 @@
-class Solution {
-    class Hashing {
+class Solution {class Hashing {
         String s;
         int n;
         int primesCount;
@@ -62,8 +61,8 @@ class Solution {
             long result = 1;
             a %= mod;
             while (b > 0) {
-                if ((b & 1) == 1) 
-                result = (result * a) % mod;
+                if ((b & 1) == 1)
+                    result = (result * a) % mod;
                 a = (a * a) % mod;
                 b >>= 1;
             }
@@ -85,52 +84,10 @@ class Solution {
                 ans += 2;
                 length = 0;
             }else
-            length++;
+                length++;
         }
         if(l <= r)
-        ans++;
+            ans++;
         return ans;
     }
-    /*List<Integer> ll = new ArrayList<>();
-        List<Integer> rl = new ArrayList<>();
-        ll.add(0);
-        rl.add(text.length()-1);
-        int l = 0;
-        int r = text.length()-1;
-        while(l < r){
-            List<Long> lh = hs.substringHash(ll.get(ll.size()-1) , l);
-            List<Long> rh = hs.substringHash(r , rl.get(rl.size()-1));
-            for(int i = 0; i < lh.size(); i++){
-                if(rh.contains(lh.get(i))){
-                    ll.add(l+1);
-                    rl.add(r-1);
-                }
-            }
-            l++;
-            r--;
-        }
-        //System.out.println(ll);
-        //System.out.println(rl);
-        Set<Integer> ans = new HashSet<>();
-        ans.addAll(ll);
-        ans.addAll(rl);
-        return ans.size()-1;*/
-        /*int l = 0; 
-        int r = text.length()-1;
-        int length = 0;
-        int ans = 0;
-        while(l+length <= r-length){
-            List<Long> lh = hs.substringHash(l , l+length);
-            List<Long> rh = hs.substringHash(r-length , r);
-            if(lh.equals(rh)){
-                l = l+length+1;
-                r = r-length-1;
-                ans += 2;
-                length = 0;
-            }else
-            length++;
-        }
-        if(l <= r)
-        ans++;
-        return Math.min(ans , text.length());*/
 }
