4cb217ed97113551e631e8205286a5ba1ed87fe0
HarshMehar50 <harshmehar4955@gmail.com>
2025-06-11 23:24:57 +0530

    Time: 125 ms (44.75%), Space: 221.6 MB (20.55%) - LeetHub

--- 1952-minimum-sideway-jumps/1952-minimum-sideway-jumps.java
+++ 1952-minimum-sideway-jumps/1952-minimum-sideway-jumps.java
@@ -1,20 +1,28 @@
 class Solution {
-    int solve(int[] obstacles , int cl , int cp){
+    int solve(int[] obstacles , int cl , int cp , int[][] dp){
         if(cp == obstacles.length-1){
             return 0;
         }
+        if(dp[cl][cp] != -1){
+            return dp[cl][cp];
+        }
         int ans = Integer.MAX_VALUE;
         if(obstacles[cp+1] != cl){
-            return solve(obstacles , cl , cp+1);
+            return solve(obstacles , cl , cp+1 , dp);
         }else{
             for(int i = 1; i <= 3; i++){
                 if(cl != i && obstacles[cp] != i)
-                ans = Math.min(ans , 1+solve(obstacles , i , cp));
+                    ans = Math.min(ans , 1+solve(obstacles , i , cp , dp));
+                dp[cl][cp] = ans;
             }
         }
-        return ans;
+        return dp[cl][cp];
     }
     public int minSideJumps(int[] obstacles) {
-        return solve(obstacles, 2 , 0);
+        int[][] dp = new int[4][obstacles.length];
+        for(int i = 0; i < 4; i++){
+            Arrays.fill(dp[i] , -1);
+        }
+        return solve(obstacles, 2 , 0 , dp);
     }
 }
