aa28c38cad9729dc50e8517cee06185630ea992f
HarshMehar50 <harshmehar4955@gmail.com>
2025-06-11 23:06:28 +0530

    Time: 333 ms (9.76%), Space: 124.5 MB (9.76%) - LeetHub

--- 3296-minimum-time-to-revert-word-to-initial-state-ii/3296-minimum-time-to-revert-word-to-initial-state-ii.java
+++ 3296-minimum-time-to-revert-word-to-initial-state-ii/3296-minimum-time-to-revert-word-to-initial-state-ii.java
@@ -1,5 +1,4 @@
-class Solution {
-    class Hashing {
+class Solution {class Hashing {
         String s;
         int n;
         int primesCount;
@@ -62,8 +61,8 @@ class Solution {
             long result = 1;
             a %= mod;
             while (b > 0) {
-                if ((b & 1) == 1) 
-                result = (result * a) % mod;
+                if ((b & 1) == 1)
+                    result = (result * a) % mod;
                 a = (a * a) % mod;
                 b >>= 1;
             }
@@ -72,7 +71,7 @@ class Solution {
     }
     public int minimumTimeToInitialState(String word, int k) {
         if(k == word.length())
-        return 1;
+            return 1;
         Hashing h = new Hashing(word);
         /*int l = 0;
         for(int i = 0; i < word.length()-1; i++){
@@ -95,11 +94,11 @@ class Solution {
             List<Long> l1 = h.substringHash(0 , l-1);
             List<Long> l2 = h.substringHash(s , word.length()-1);
             if(l1.equals(l2))
-            return i;
+                return i;
         }
         int ans = word.length()/k;
         if(word.length()%k != 0)
-        ans++;
+            ans++;
         return ans;
     }
 }
