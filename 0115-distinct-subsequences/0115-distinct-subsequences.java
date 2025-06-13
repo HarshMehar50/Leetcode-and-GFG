40b2d15bf87f23ff995b9d811b8d40d0327d777c
HarshMehar50 <harshmehar4955@gmail.com>
2025-06-13 23:22:05 +0530

    Time: 26 ms (8.96%), Space: 51 MB (75.24%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.java
+++ 0115-distinct-subsequences/0115-distinct-subsequences.java
@@ -1,16 +1,16 @@
 class Solution {
     int solve(String s , String t , int i , int j , int[][] dp){
         if(j < 0)
-        return 1;
+            return 1;
         if(i < 0)
-        return 0;
+            return 0;
         if(dp[i][j] != -1)
-        return dp[i][j];
+            return dp[i][j];
         int ans = 0;
         if(s.charAt(i) == t.charAt(j))
-        ans = solve(s , t , i-1 , j-1 , dp)+solve(s , t , i-1 , j , dp);
+            ans = solve(s , t , i-1 , j-1 , dp)+solve(s , t , i-1 , j , dp);
         else
-        ans = solve(s , t , i-1 , j , dp);
+            ans = solve(s , t , i-1 , j , dp);
         dp[i][j] = ans;
         return dp[i][j];
     }
