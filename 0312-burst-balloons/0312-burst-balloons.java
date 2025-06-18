0837d343ad8f43eb1e50f5a0f573a33cece05182
HarshMehar50 <harshmehar4955@gmail.com>
2025-06-19 00:00:35 +0530

    Time: 113 ms (29.33%), Space: 43.2 MB (16.42%) - LeetHub

--- 0312-burst-balloons/0312-burst-balloons.java
+++ 0312-burst-balloons/0312-burst-balloons.java
@@ -1,13 +1,16 @@
 class Solution {
-    int solve(int[] nums , int l , int r){
+    int solve(int[] nums , int l , int r , int[][] dp){
         if(l > r)
-        return 0;
+            return 0;
+        if(dp[l][r] != -1)
+            return dp[l][r];
         int ans = Integer.MIN_VALUE;
         for(int i = l; i <= r; i++){
             int s = nums[l-1]*nums[i]*nums[r+1];
-            ans = Math.max(ans , s+solve(nums , l , i-1)+solve(nums , i+1 , r));
+            ans = Math.max(ans , s+solve(nums , l , i-1 , dp)+solve(nums , i+1 , r , dp));
         }
-        return ans;
+        dp[l][r] = ans;
+        return dp[l][r];
     }
     public int maxCoins(int[] nums) {
         int[] a = new int[nums.length+2];
@@ -16,6 +19,10 @@ class Solution {
         }
         a[0] = 1;
         a[a.length-1] = 1;
-        return solve(a , 1 , nums.length);
+        int[][] dp = new int[a.length][a.length];
+        for(int i = 0; i < dp.length; i++){
+            Arrays.fill(dp[i] , -1);
+        }
+        return solve(a , 1 , nums.length , dp);
     }
 }
