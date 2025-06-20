fb8cd7fca381a79663bbc4da63d38a640a43ce1a
HarshMehar50 <harshmehar4955@gmail.com>
2025-06-20 13:37:40 +0530

    Time: 94 ms (85%), Space: 57.2 MB (70%) - LeetHub

--- 1947-number-of-different-subsequences-gcds/1947-number-of-different-subsequences-gcds.java
+++ 1947-number-of-different-subsequences-gcds/1947-number-of-different-subsequences-gcds.java
@@ -1,6 +1,11 @@
 class Solution {
+    int gcd(int a, int b) {
+        if(b == 0)
+        return Math.abs(a);
+        return gcd(b , a%b);
+    }
     public int countDifferentSubsequenceGCDs(int[] nums) {
-        int ans = 1;
+        /*int ans = 1;
         int max = 0;
         for(int i = 0; i < nums.length; i++){
             max = Math.max(max , nums[i]);
@@ -17,8 +22,32 @@ class Solution {
             if(fc == 0 || (fc == 1 && (Arrays.binarySearch(nums , i) < 0 || Arrays.binarySearch(nums , i) >= nums.length)))
             ans--;
         }
-        if(nums[0] == 1)
-        ans--;
+        return ans;*/
+        
+        int max = 0;
+        for(int i = 0; i < nums.length; i++){
+            max = Math.max(nums[i] , max);
+        }
+        boolean[] present = new boolean[max+1];
+        for(int i = 0; i < nums.length; i++){
+            present[nums[i]] = true;
+        }
+        int ans = 0;
+        for(int i = 1; i <= max; i++){
+            int gcd = 0;
+            for(int j =i; j <= max; j += i){
+                if(present[j]){
+                    if(gcd == 0)
+                    gcd = j;
+                    else
+                    gcd = gcd(gcd , j);
+                    if(gcd == i){
+                        ans++;
+                        break;
+                    }
+                }
+            }
+        }
         return ans;
     }
 }
