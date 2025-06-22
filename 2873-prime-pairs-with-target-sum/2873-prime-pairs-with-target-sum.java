12c776f2886c30e1c8335f73c96cd183583704bb
HarshMehar50 <harshmehar4955@gmail.com>
2025-06-22 15:29:58 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 2873-prime-pairs-with-target-sum/2873-prime-pairs-with-target-sum.java
@@ -0,0 +1,32 @@
+class Solution {
+    boolean[] seive(int n){
+        boolean[] p = new boolean[n+1];
+        Arrays.fill(p , true);
+        p[0] = false;
+        p[1] = false;
+        for(int i = 2; i <= n; i++){
+            if(p[i]){
+                if((long)((long)i*(long)i) <= (long)n){
+                for(int j = i*i; j <= n; j += i){
+                    p[j] = false;
+                }
+                }
+            }
+        }
+        return p;
+    }
+    public List<List<Integer>> findPrimePairs(int n) {
+        boolean[] prime = seive(n);
+        List<List<Integer>> ans = new ArrayList<>();
+        for(int i = 2; i <= n/2; i++){
+            if(prime[i] && prime[n-i]){
+                List<Integer> inner = new ArrayList<>();
+                inner.add(Math.min(i , n-i));
+                inner.add(Math.max(i , n-i));
+                if(!ans.contains(inner))
+                ans.add(inner);
+            }
+        }
+        return ans;
+    }
+}
