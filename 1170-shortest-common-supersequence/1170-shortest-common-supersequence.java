ddb908700c78f029be4082a9f1a639d77774b7b9
HarshMehar50 <harshmehar4955@gmail.com>
2025-06-23 23:51:04 +0530

    Time: 274 ms (5.23%), Space: 160.8 MB (5.46%) - LeetHub

--- 1170-shortest-common-supersequence/1170-shortest-common-supersequence.java
+++ 1170-shortest-common-supersequence/1170-shortest-common-supersequence.java
@@ -1,10 +1,10 @@
 class Solution {
     String solve(String s1 , String s2 , int i , int j , HashMap<String , String> dp){
         if(i >= s1.length() || j >= s2.length())
-        return "";
+            return "";
         String key = i+" "+j;
         if(dp.containsKey(key))
-        return dp.get(key);
+            return dp.get(key);
         if(s1.charAt(i) == s2.charAt(j)){
             String ans = s1.charAt(i)+solve(s1 , s2 , i+1 , j+1 , dp);
             dp.put(key , ans);
@@ -14,9 +14,9 @@ class Solution {
             String si2 = solve(s1 , s2 , i , j+1 , dp);
             String ans = "";
             if(si1.length() > si2.length())
-            ans = si1;
+                ans = si1;
             else
-            ans = si2;
+                ans = si2;
             dp.put(key , ans);
             return ans;
         }
@@ -26,7 +26,7 @@ class Solution {
         String c = solve(str1 , str2 , 0 , 0 , dp);
         String ans = "";
         if(c.length() == 0)
-        return str1+str2;
+            return str1+str2;
         int j = 0;
         int k = 0;
         for(int i = 0; i < c.length(); i++){
