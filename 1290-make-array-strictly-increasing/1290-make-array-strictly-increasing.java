b6003c05120c07372e4f75ca41e1e06d35785a4f
HarshMehar50 <harshmehar4955@gmail.com>
2025-06-25 00:25:12 +0530

    Time: 264 ms (32.98%), Space: 116.9 MB (19.89%) - LeetHub

--- 1290-make-array-strictly-increasing/1290-make-array-strictly-increasing.java
+++ 1290-make-array-strictly-increasing/1290-make-array-strictly-increasing.java
@@ -5,24 +5,24 @@ class Solution {
         while(s <= e){
             int m = s+(e-s)/2;
             if(a[m] > t)
-            e = m-1;
+                e = m-1;
             else
-            s = m+1;
+                s = m+1;
         }
         return s;
     }
     int solve(int[] arr1 , int[] arr2 , int c , int p , HashMap<Pair<Integer , Integer> , Integer> dp){
         if(c >= arr1.length)
-        return 0;
+            return 0;
         if(dp.containsKey(new Pair<>(c , p)))
-        return dp.get(new Pair<>(c , p));
+            return dp.get(new Pair<>(c , p));
         int exclude = (int)(1e9)+1;
         int include = (int)(1e9)+1;
         if(arr1[c] > p)
-        exclude = solve(arr1 , arr2 , c+1 , arr1[c] , dp);
+            exclude = solve(arr1 , arr2 , c+1 , arr1[c] , dp);
         int index = ceil(arr2 , p);
         if(index != arr2.length)
-        include = 1+solve(arr1 , arr2 , c+1 , arr2[index] , dp);
+            include = 1+solve(arr1 , arr2 , c+1 , arr2[index] , dp);
         int ans = Math.min(include , exclude);
         dp.put(new Pair<>(c , p) , ans);
         return dp.get(new Pair<>(c , p));
@@ -32,8 +32,8 @@ class Solution {
         HashMap<Pair<Integer , Integer> , Integer> dp = new HashMap<>();
         int ans = solve(arr1 , arr2 , 0 , -1 , dp);
         if(ans == (int)(1e9)+1)
-        return -1;
+            return -1;
         else
-        return ans;
+            return ans;
     }
 }
