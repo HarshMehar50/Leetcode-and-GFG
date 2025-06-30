18df59e2e784ece2d47120fb2ab2281c7087da93
HarshMehar50 <harshmehar4955@gmail.com>
2025-06-30 11:31:41 +0530

    Time: 0 ms (100%), Space: 40 MB (100%) - LeetHub

---
+++ 2635-check-if-point-is-reachable/2635-check-if-point-is-reachable.java
@@ -0,0 +1,16 @@
+class Solution {
+    int gcd(int a , int b){
+        if(b == 0)
+        return Math.abs(a);
+        return gcd(b , a%b);
+    }
+    public boolean isReachable(int targetX, int targetY) {
+        if((targetX&(targetX-1)) == 0 || (targetY&(targetY-1)) == 0 || targetX == 1 || targetY == 1)
+        return true;
+        //System.out.println(gcd(targetX , targetY));
+        int g = gcd(targetX , targetY);
+        if(g == 1 || (g&(g-1)) == 0)
+        return true;
+        return false;
+    }
+}
