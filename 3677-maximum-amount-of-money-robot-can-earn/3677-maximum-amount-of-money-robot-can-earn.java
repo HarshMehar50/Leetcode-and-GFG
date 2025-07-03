ca3cf74e886aab921c3e77073439b388b108956e
HarshMehar50 <harshmehar4955@gmail.com>
2025-07-04 00:27:05 +0530

    Time: 219 ms (19.23%), Space: 91.2 MB (29.49%) - LeetHub

--- 3677-maximum-amount-of-money-robot-can-earn/3677-maximum-amount-of-money-robot-can-earn.java
+++ 3677-maximum-amount-of-money-robot-can-earn/3677-maximum-amount-of-money-robot-can-earn.java
@@ -1,15 +1,15 @@
 class Solution {
-    int solve(int[][] coins , int i , int j , int k , int[][][] dp){
+    int solve(int[][] coins , int i , int j , int k ,  int[][][] dp){
         if(i >= coins.length || j >= coins[0].length)
-        return (int)(-1e9);
+            return (int)(-1e9);
         if(i == coins.length-1 && j == coins[0].length-1){
             if(k > 0 && coins[i][j] < 0)
-            return 0;
+                return 0;
             else
-            return coins[i][j];
+                return coins[i][j];
         }
         if(dp[i][j][k] != (int)(-1e9))
-        return dp[i][j][k];
+            return dp[i][j][k];
         int rightp = (int)(-1e9);
         int downp = (int)(-1e9);
         int rightc = (int)(-1e9);
@@ -20,8 +20,8 @@ class Solution {
         }
         if(coins[i][j] < 0){
             if(k > 0){
-            rightc = solve(coins , i , j+1 , k-1 , dp);
-            downc = solve(coins , i+1 , j , k-1 , dp);
+                rightc = solve(coins , i , j+1 , k-1 , dp);
+                downc = solve(coins , i+1 , j , k-1 , dp);
             }
             rightc = Math.max(rightc , coins[i][j]+solve(coins , i+1 , j , k , dp));
             downc = Math.max(downc , coins[i][j]+solve(coins , i , j+1 , k , dp));
