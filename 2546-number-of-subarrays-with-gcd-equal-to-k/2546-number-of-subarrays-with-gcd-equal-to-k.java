9778c036d440bb9056fd54eb65b2025f12bb9a29
HarshMehar50 <harshmehar4955@gmail.com>
2025-07-06 00:49:32 +0530

    Time: 6 ms (65.68%), Space: 42 MB (83.43%) - LeetHub

--- 2546-number-of-subarrays-with-gcd-equal-to-k/2546-number-of-subarrays-with-gcd-equal-to-k.java
+++ 2546-number-of-subarrays-with-gcd-equal-to-k/2546-number-of-subarrays-with-gcd-equal-to-k.java
@@ -4,24 +4,16 @@ class Solution {
         return Math.abs(a);
         return gcd(b , a%b);
     }
-    int subgcd(int[] nums , int l , int r){
-        int g = nums[l];
-        for(int i = l+1; i <= r; i++){
-            g = gcd(g , nums[i]);
-        }
-        return g;
-    }
     public int subarrayGCD(int[] nums, int k) {
         int ans = 0;
-        for(int l = 1; l <= nums.length; l++){
-            for(int i = 0; i+l-1 < nums.length; i++){
-                if(l == 1 && nums[i] == k)
+        for(int i = 0; i < nums.length; i++){
+            int g = 0;
+            for(int j = i; j < nums.length; j++){
+                g = gcd(g , nums[j]);
+                if(g < k)
+                break;
+                if(g == k)
                 ans++;
-                else{
-                    int g = subgcd(nums , i , i+l-1);
-                    if(g == k)
-                    ans++;
-                }
             }
         }
         return ans;
