a0f1deb0a0a600ea5945c78feb71a73eb388297e
HarshMehar50 <harshmehar4955@gmail.com>
2025-07-08 11:05:01 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Word Ladder I/word-ladder-i.java
+++ Difficulty: Hard/Word Ladder I/word-ladder-i.java
@@ -1,31 +1,3 @@
-//{ Driver Code Starts
-import java.util.*;
-import java.lang.*;
-import java.io.*;
-class GFG
-{
-    public static void main(String[] args) throws IOException
-    {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int T = Integer.parseInt(br.readLine().trim());
-        while(T-->0)
-        {
-            int n = Integer.parseInt(br.readLine().trim());
-            String[] wordList = new String[n];
-            for(int i = 0; i < n; i++){
-                wordList[i] = br.readLine().trim();
-            }
-            String startWord, targetWord;
-            startWord = br.readLine().trim();
-            targetWord = br.readLine().trim();
-            Solution obj = new Solution();
-            int ans = obj.wordLadderLength(startWord, targetWord, wordList);
-            System.out.println(ans);
-       }
-    }
-}
-
-// } Driver Code Ends
 
 
 class Solution
