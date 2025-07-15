8761eaa979b914a8a221ddafe9ff22683e1b912b
HarshMehar50 <harshmehar4955@gmail.com>
2025-07-16 01:23:29 +0530

    Time: 81 ms (76.52%), Space: 45.7 MB (88.7%) - LeetHub

--- 1871-palindrome-partitioning-iv/1871-palindrome-partitioning-iv.java
+++ 1871-palindrome-partitioning-iv/1871-palindrome-partitioning-iv.java
@@ -1,44 +1,23 @@
 class Solution {
-    boolean check(String st , int s , int e){
-        if(s == e)
-        return true;
-        while(s < e){
-            if(st.charAt(s) != st.charAt(e))
-            return false;
-            s++;
-            e--;
-        }
-        return true;
-    }
-    boolean solve(String s , int l , int r , int[][] dp){
-        if(l > r)
-        return false;
-        if(check(s , 0 , l-1) && check(s , l , r) && check(s , r+1 , s.length()-1))
-        return true;
-        if(dp[l][r] != -1){
-            if(dp[l][r] == 1)
-            return true;
-            else
-            return false;
-        }
-        boolean ans = false;
-        for(int i = l+1; i < r; i++){
-            ans = ans|solve(s , i , r , dp);
-        }
-        for(int i = r-1; i >= l; i--){
-            ans = ans|solve(s , l , i , dp);
-        }
-        if(ans)
-        dp[l][r] = 1;
-        else
-        dp[l][r] = 0;
-        return ans;
-    }
     public boolean checkPartitioning(String s) {
-        int[][] dp = new int[s.length()][s.length()];
-        for(int i = 0; i < dp.length; i++){
-            Arrays.fill(dp[i] , -1);
+        boolean[][] dp = new boolean[s.length()][s.length()];
+        for(int l = 1; l <= s.length(); l++){
+            for(int i = 0; i+l-1 < s.length(); i++){
+                int j = i+l-1;
+                if(i == j)
+                dp[i][j] = true;
+                else if(i+1 == j && s.charAt(i) == s.charAt(j))
+                dp[i][j] = true;
+                else
+                dp[i][j] = dp[i+1][j-1]&&(s.charAt(i) == s.charAt(j));
+            }
         }
-        return solve(s , 1 , s.length()-2 , dp);
+        for(int i = 1; i < s.length()-1; i++){
+            for(int j = s.length()-2; j >= i; j--){
+                if(dp[i][j] && dp[0][i-1] && dp[j+1][s.length()-1])
+                return true;
+            }
+        }
+        return false;
     }
 }
