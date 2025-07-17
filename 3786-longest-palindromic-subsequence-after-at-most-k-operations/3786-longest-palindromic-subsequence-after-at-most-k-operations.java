bd427910860530b232d6ecfa8eeb579c62d80e33
HarshMehar50 <harshmehar4955@gmail.com>
2025-07-18 00:43:32 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 3786-longest-palindromic-subsequence-after-at-most-k-operations/3786-longest-palindromic-subsequence-after-at-most-k-operations.java
+++ 3786-longest-palindromic-subsequence-after-at-most-k-operations/3786-longest-palindromic-subsequence-after-at-most-k-operations.java
@@ -1,5 +1,5 @@
 class Solution {
-    /*int solve(String s , String r , int k , int i , int j){
+    int solve(String s , String r , int k , int i , int j){
         if(i >= s.length() || j >= r.length())
         return 0;
         if(s.charAt(i) == r.charAt(j))
@@ -14,29 +14,37 @@ class Solution {
             int ans = Math.max(includeop , Math.max(m1 , m2));
             return ans;
         }
-    }*/
-    int solve(String s , int k , int i , int j){
+    }
+    int solve(String s , int k , int i , int j , int[][][] dp){
         if(i > j)
-        return 0;
+            return 0;
         if(i == j)
-        return 1;
+            return 1;
+        if(dp[i][j][k] != -1)
+            return dp[i][j][k];
         if(s.charAt(i) == s.charAt(j))
-        return 2+solve(s , k , i+1 , j-1);
+            return 2+solve(s , k , i+1 , j-1 , dp);
         else{
             int d = Math.min(Math.abs((int)(s.charAt(i)-s.charAt(j))) , 26-Math.abs((int)(s.charAt(i)-s.charAt(j))));
             int includeop = 0;
             if(k-d >= 0)
-            includeop = 2+solve(s , k-d , i+1 , j-1);
-            int m1 = solve(s , k , i+1 , j);
-            int m2 = solve(s , k , i , j-1);
-            int ans = Math.max(includeop , Math.max(m1 , m2)); 
-            return ans;
+                includeop = 2+solve(s , k-d , i+1 , j-1 , dp);
+            int m1 = solve(s , k , i+1 , j , dp);
+            int m2 = solve(s , k , i , j-1 , dp);
+            int ans = Math.max(includeop , Math.max(m1 , m2));
+            dp[i][j][k] = ans;
+            return dp[i][j][k];
         }
     }
     public int longestPalindromicSubsequence(String s, int k) {
-        //String r = new StringBuilder(s).reverse().toString();
-        //return solve(s , r , k , 0 , 0);
-        
-        return solve(s , k , 0 , s.length()-1); 
+        String r = new StringBuilder(s).reverse().toString();
+        return solve(s , r , k , 0 , 0);
+        /*int[][][] dp = new int[s.length()][s.length()][k+1];
+        for(int[][] a : dp){
+            for(int i = 0; i < a.length; i++){
+                Arrays.fill(a[i] , -1);
+            }
+        }
+        return solve(s , k , 0 , s.length()-1 , dp);*/
     }
 }
