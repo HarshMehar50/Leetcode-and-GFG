4212d9642c22f355f27029ba6db8af1721ceff09
HarshMehar50 <harshmehar4955@gmail.com>
2025-07-18 00:51:37 +0530

    Time: 365 ms (5.14%), Space: 278 MB (5.14%) - LeetHub

--- 1896-maximum-score-from-performing-multiplication-operations/1896-maximum-score-from-performing-multiplication-operations.java
+++ 1896-maximum-score-from-performing-multiplication-operations/1896-maximum-score-from-performing-multiplication-operations.java
@@ -1,17 +1,18 @@
 class Solution {
     int solve(int[] nums , int[] multipliers , int l , int r , int i , int[][][] dp){
         if(i >= multipliers.length || l > r)
-        return 0;
+            return 0;
         int r1 = nums.length-1-r;
         int rf = multipliers.length-1-r1;
         if(dp[l][rf][i] != -1)
-        return dp[l][rf][i];
+            return dp[l][rf][i];
         int ans = 0;
         ans = Math.max((nums[l]*multipliers[i])+solve(nums , multipliers , l+1 , r , i+1 , dp) , (nums[r]*multipliers[i])+solve(nums , multipliers , l , r-1 , i+1 , dp));
         dp[l][rf][i] = ans;
         return dp[l][rf][i];
     }
     public int maximumScore(int[] nums, int[] multipliers) {
+        // dp(l , r , i) = max(m[i]*a[l]+dp(l+1 , r , i+1) , m[i]*a[r]+dp(l , r-1 , i+1))
         int[][][] dp = new int[multipliers.length][multipliers.length][multipliers.length];
         for(int[][] a : dp){
             for(int i = 0; i < a.length; i++){
