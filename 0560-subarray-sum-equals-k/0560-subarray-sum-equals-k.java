49a9e316c8841d433d3c086f4f2af7f27a2bded6
HarshMehar50 <harshmehar4955@gmail.com>
2025-07-23 10:31:01 +0530

    Time: 2629 ms (5%), Space: 46.3 MB (83.92%) - LeetHub

---
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
@@ -0,0 +1,38 @@
+class Solution {
+    public int subarraySum(int[] nums, int k) {
+        int[] ps = new int[nums.length];
+        ps[0] = nums[0];
+        for(int i = 1; i < nums.length; i++){
+            ps[i] = ps[i-1]+nums[i];
+        }
+        /*HashMap<Integer , Integer> map = new HashMap<>();
+        for(int i = 0; i < nums.length; i++){
+            map.put(ps[i] , Math.min(map.getOrDefault(ps[i] , Integer.MAX_VALUE) , i));
+        }
+        int ans = 0;
+        for(int i = 0; i < nums.length; i++){
+            if(ps[i] == k)
+            ans++;
+            else if(ps[i] > k){
+                if(map.containsKey(ps[i]-k) && map.get(ps[i]-k) < i)
+                ans++;
+            }else{
+                if(map.containsKey(k-ps[i]) && map.get(k-ps[i]) > i)
+                ans++;
+            }
+        }*/
+        int ans = 0;
+        for(int l = 1; l <= nums.length; l++){
+            for(int i = 0; i+l-1 < nums.length; i++){
+                int ss = 0;
+                if(i == 0)
+                ss = ps[i+l-1];
+                else
+                ss = ps[i+l-1]-ps[i-1];
+                if(ss == k)
+                ans++;
+            }
+        }
+        return ans;
+    }
+}
