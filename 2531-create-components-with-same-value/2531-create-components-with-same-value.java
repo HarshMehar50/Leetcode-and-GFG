836dcb2cf864ba3e188e7c5bd1603fded7e9f12b
HarshMehar50 <harshmehar4955@gmail.com>
2025-07-25 10:43:05 +0530

    Time: 240 ms (10.71%), Space: 61 MB (57.14%) - LeetHub

--- 2531-create-components-with-same-value/2531-create-components-with-same-value.java
+++ 2531-create-components-with-same-value/2531-create-components-with-same-value.java
@@ -1,13 +1,5 @@
 class Solution {
-    void DFS(HashMap<Integer , List<Integer>> adj , int[] nums , int node , int parent , int[] sts){
-        sts[node] = nums[node];
-        for(Integer x : adj.get(node)){
-            if(x != parent){
-                DFS(adj , nums , x , node , sts);
-                sts[node] += sts[x];
-            }
-        }
-    }
+    
     int DFS1(HashMap<Integer , List<Integer>> adj , int[] nums , int cs , int nc , int[] c , int node , int parent){
         int s = nums[node];
         for(Integer x : adj.get(node)){
@@ -30,16 +22,16 @@ class Solution {
     }
     public int componentValue(int[] nums, int[][] edges) {
         HashMap<Integer , List<Integer>> adj = new HashMap<>();
+        int s = 0;
         for(int i = 0; i < nums.length; i++){
             adj.put(i , new ArrayList<>());
+            s += nums[i];
         }
         for(int i = 0; i < edges.length; i++){
             adj.get(edges[i][0]).add(edges[i][1]);
             adj.get(edges[i][1]).add(edges[i][0]);
         }
-        int[] sts = new int[nums.length];
-        DFS(adj , nums , 0 , -1 , sts);
-        int s = sts[0];
+        
         for(int i = s; i >= 1; i--){
             if(s%i == 0)
             if(check(adj , nums , s/i , i))
