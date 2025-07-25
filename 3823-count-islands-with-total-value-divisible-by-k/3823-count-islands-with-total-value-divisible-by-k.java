d1e4d297b5b824f2ba44236ef1be2cf8b3bcdcd9
HarshMehar50 <harshmehar4955@gmail.com>
2025-07-26 00:46:44 +0530

    Time: 26 ms (25.01%), Space: 69.3 MB (69.22%) - LeetHub

--- 3823-count-islands-with-total-value-divisible-by-k/3823-count-islands-with-total-value-divisible-by-k.java
+++ 3823-count-islands-with-total-value-divisible-by-k/3823-count-islands-with-total-value-divisible-by-k.java
@@ -1,14 +1,14 @@
 class Solution {
     int[] dR = {1 , 0 , -1 , 0};
     int[] dC = {0 , 1 , 0 , -1};
-    void DFS(int[][] grid , boolean[][] visited , int r , int c , int[] a){
+    void DFS(int[][] grid , boolean[][] visited , int k , int r , int c , int[] a){
         visited[r][c] = true;
         a[0] += grid[r][c];
         for(int i = 0; i < 4; i++){
             int nr = r+dR[i];
             int nc = c+dC[i];
             if(nr < grid.length && nr >= 0 && nc < grid[0].length && nc >= 0 && !visited[nr][nc] && grid[nr][nc] != 0)
-                DFS(grid , visited , nr , nc , a);
+            DFS(grid , visited , k , nr , nc , a);
         }
     }
     public int countIslands(int[][] grid, int k) {
@@ -18,16 +18,16 @@ class Solution {
             for(int j = 0; j < grid[0].length; j++){
                 if(!visited[i][j] && grid[i][j] != 0){
                     int[] a = {0};
-                    DFS(grid , visited , i , j , a);
+                    DFS(grid , visited , k , i , j , a);
                     l.add(a[0]);
                 }
             }
         }
         int ans = 0;
-        for(int i = 0; i < l.size(); i++){
-            if(l.get(i)%k == 0)
-                ans++;
+        for(Integer x : l){
+            if(x%k == 0)
+            ans++;
         }
-        return  ans;
+        return ans;
     }
 }
