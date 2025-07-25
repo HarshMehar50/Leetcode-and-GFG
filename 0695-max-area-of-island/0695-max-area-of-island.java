7e75c1a766e3d0443b20db2143f36fc5f68cf88d
HarshMehar50 <harshmehar4955@gmail.com>
2025-07-25 12:56:52 +0530

    Time: 2 ms (68.12%), Space: 44.9 MB (33.23%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.java
+++ 0695-max-area-of-island/0695-max-area-of-island.java
@@ -1,26 +1,25 @@
 class Solution {
-    int DFS(int r , int c , boolean[][] visited , int[][] grid , int[] dR , int[] dC){
-        int count = 1;
+    int[] dR = {1 , 0 , -1 , 0};
+    int[] dC = {0 , 1 , 0 , -1};
+    void DFS(int[][] grid , boolean[][] visited , int r , int c , int[] a){
         visited[r][c] = true;
+        a[0] += grid[r][c];
         for(int i = 0; i < 4; i++){
             int nr = r+dR[i];
             int nc = c+dC[i];
-            if(nr < grid.length && nr >= 0 && nc < grid[0].length && nc >= 0){
-                if(!visited[nr][nc] && grid[nr][nc] == 1)
-                count += DFS(nr , nc , visited , grid , dR , dC);
-            }
+            if(nr >= 0 && nr < grid.length && nc >= 0 && nc < grid[0].length && !visited[nr][nc] && grid[nr][nc] == 1)
+            DFS(grid , visited , nr , nc , a);
         }
-        return count;
     }
     public int maxAreaOfIsland(int[][] grid) {
         int ans = 0;
-        int[] dR = {1 , 0 , -1 , 0};
-        int[] dC = {0 , -1 , 0 , 1};
         boolean[][] visited = new boolean[grid.length][grid[0].length];
         for(int i = 0; i < grid.length; i++){
             for(int j = 0; j < grid[0].length; j++){
-                if(!visited[i][j] && grid[i][j] == 1){
-                    ans = Math.max(ans , DFS(i , j , visited , grid , dR , dC));
+                if(grid[i][j] == 1 && !visited[i][j]){
+                    int[] a = {0};
+                    DFS(grid , visited , i , j , a);
+                    ans = Math.max(ans , a[0]);
                 }
             }
         }
