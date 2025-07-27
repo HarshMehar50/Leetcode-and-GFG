aef7bfcf63db0cb884ffef76114024a345b7ba1f
HarshMehar50 <harshmehar4955@gmail.com>
2025-07-27 16:15:41 +0530

    Time: 131 ms (92.39%), Space: 44.3 MB (91.3%) - LeetHub

--- 1024-triples-with-bitwise-and-equal-to-zero/1024-triples-with-bitwise-and-equal-to-zero.java
+++ 1024-triples-with-bitwise-and-equal-to-zero/1024-triples-with-bitwise-and-equal-to-zero.java
@@ -1,16 +1,42 @@
 class Solution {
     public int countTriplets(int[] nums) {
-        int[] maskc = new int[(int)(1<<16)];
+        /*Arrays.sort(nums);
+        int ans = 0;
         for(int i = 0; i < nums.length; i++){
             for(int j = 0; j < nums.length; j++){
-                maskc[nums[i]&nums[j]]++;
+                int a = nums[i]&nums[j];
+                int b = 0;
+                int mb = 0;
+                for(int k = 0; k <= 16; k++){
+                    if((a&(1<<k)) != 0){
+                        mb = i;
+                    }
+                }
+                for(int k = 0; k <= mb; k++){
+                    if((a&(1<<i)) == 0)
+                    b += (1<<k);
+                }
+                int bs = Arrays.binarySearch(nums , b);
+                if(bs < nums.length && bs >= 0)
+                ans++;
             }
-        } 
-        int ans = 0;
-        for(int i = 0; i < maskc.length; i++){
+        }
+        return ans;*/
+        int max = 0;
+        for(int i = 0; i < nums.length; i++){
+            max = Math.max(max , nums[i]);
+        }
+        int[] f = new int[max+1];
+        for(int i = 0; i < nums.length; i++){
             for(int j = 0; j < nums.length; j++){
-                if((i&nums[j]) == 0)
-                ans += maskc[i];
+                f[nums[i]&nums[j]]++;
+            }
+        }
+        int ans = 0;
+        for(int i = 0; i <= max; i++){
+            for(int j = 0; j < nums.length; j++){
+                if((nums[j]&i) == 0)
+                ans += f[i];
             }
         }
         return ans;
