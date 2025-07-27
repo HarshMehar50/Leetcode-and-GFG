bbd3dd42e6cdd7a673ea324b0a742dfbd6088328
HarshMehar50 <harshmehar4955@gmail.com>
2025-07-27 11:49:03 +0530

    Time: 24 ms (86.16%), Space: 129.8 MB (65.17%) - LeetHub

---
+++ 3766-maximum-median-sum-of-subsequences-of-size-3/3766-maximum-median-sum-of-subsequences-of-size-3.java
@@ -0,0 +1,13 @@
+class Solution {
+    public long maximumMedianSum(int[] nums) {
+        Arrays.sort(nums);
+        long ans = 0;
+        boolean[] visited = new boolean[nums.length];
+        int c = 0;
+        while(c != nums.length/3){
+            ans += nums[nums.length-(2*(c+1))];
+            c++;
+        }
+        return ans;
+    }
+}
