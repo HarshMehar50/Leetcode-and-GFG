841a55dd336cfa52afd907f871d56a8481f56be4
HarshMehar50 <harshmehar4955@gmail.com>
2025-07-28 21:41:36 +0530

    Time: 37 ms (44.01%), Space: 50.8 MB (11.72%) - LeetHub

--- 0676-implement-magic-dictionary/0676-implement-magic-dictionary.java
+++ 0676-implement-magic-dictionary/0676-implement-magic-dictionary.java
@@ -19,15 +19,37 @@ class MagicDictionary {
             }
             node.wordCount++;
         }
-
+        boolean solve(String word , TrieNode node , int i , int sc){
+            if(i >= word.length()){
+                if(sc == 1 && node.wordCount >= 1)
+                return true;
+                else
+                return false;
+            }
+            if(sc > 1)
+            return false;
+            for(int j = 0; j < 26; j++){
+                if(node.children[j] != null && j == word.charAt(i)-'a'){
+                    if(solve(word , node.children[j] , i+1 , sc))
+                    return true;
+                }else if(node.children[j] != null && j != word.charAt(i)-'a'){
+                    if(solve(word , node.children[j] , i+1 , sc+1))
+                    return true;
+                }
+            }
+            return false;
+        }
         public boolean search(String word) {
-            TrieNode node = root;
+            /*TrieNode node = root;
             for (char ch : word.toCharArray()) {
                 int idx = ch - 'a';
                 if (node.children[idx] == null) return false;
                 node = node.children[idx];
             }
-            return node.wordCount > 0;
+            return node.wordCount > 0;*/
+            TrieNode node = root;
+            return solve(word , node , 0 , 0);
+
         }
 
         public boolean startsWith(String prefix) {
@@ -74,18 +96,21 @@ class MagicDictionary {
     }
 
     Trie trie;
+    Trie rtrie;
     public MagicDictionary() {
         trie = new Trie();
+        rtrie = new Trie();
     }
     
     public void buildDict(String[] dictionary) {
         for(int i = 0; i < dictionary.length; i++){
             trie.insert(dictionary[i]);
+            rtrie.insert(new StringBuilder(dictionary[i]).reverse().toString());
         }
     }
     
     public boolean search(String searchWord) {
-        for(int i = 0; i < searchWord.length(); i++){
+        /*for(int i = 0; i < searchWord.length(); i++){
             for(char c = 'a'; c <= 'z'; c++){
                 if(c == searchWord.charAt(i)) 
                 continue;
@@ -95,7 +120,24 @@ class MagicDictionary {
                 return true;
             }
         }
-        return false;
+        return false;*/
+        
+        /*String ts = searchWord.substring(1);
+        String f1 = new StringBuilder(ts).reverse().toString();
+        if(rtrie.startsWith(f1) && !trie.search(searchWord))
+        return true;
+        for(int i = 1; i < searchWord.length()-1; i++){
+            String p = searchWord.substring(0 , i);
+            String s = searchWord.substring(i+1);
+            String fs = new StringBuilder(s).reverse().toString();
+            if(trie.startsWith(p) && rtrie.startsWith(fs) && !trie.search(searchWord))
+            return true;
+        }
+        if(trie.startsWith(searchWord.substring(0 , searchWord.length()-1)) && !trie.search(searchWord))
+        return true;
+        return false;*/
+
+        return trie.search(searchWord);
     }
 }
 
