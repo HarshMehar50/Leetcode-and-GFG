5d9ec4a12444dd881e77c9cbf6eaed66ba224f08
HarshMehar50 <harshmehar4955@gmail.com>
2025-07-28 23:53:31 +0530

    Time: 72 ms (22.18%), Space: 41.5 MB (32.62%) - LeetHub

--- 2170-count-number-of-maximum-bitwise-or-subsets/2170-count-number-of-maximum-bitwise-or-subsets.java
+++ 2170-count-number-of-maximum-bitwise-or-subsets/2170-count-number-of-maximum-bitwise-or-subsets.java
@@ -5,7 +5,7 @@ class Solution {
             maxor = maxor|nums[i];
         }
         int ans = 0;
-        for(int mask = 1; mask < (1<<nums.length); mask++){
+        for(int mask = 0; mask < (1<<nums.length); mask++){
             int ssor = 0;
             for(int i = 0; i < nums.length; i++){
                 if((mask&(1<<i)) != 0)
