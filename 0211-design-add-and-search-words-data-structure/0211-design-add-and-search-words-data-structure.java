b73ee0e69c164bd6c6beb76fa3b8eb9153e94b36
HarshMehar50 <harshmehar4955@gmail.com>
2025-07-29 09:26:07 +0530

    Time: 394 ms (5.49%), Space: 92.6 MB (72.78%) - LeetHub

---
+++ 0211-design-add-and-search-words-data-structure/0211-design-add-and-search-words-data-structure.java
@@ -0,0 +1,74 @@
+class WordDictionary {
+    class TrieNode {
+        TrieNode[] children = new TrieNode[26];
+        int wordCount = 0;
+        int prefixCount = 0;
+    }
+    class Trie {
+        TrieNode root = new TrieNode();
+
+        public void insert(String word) {
+            TrieNode node = root;
+            for (char ch : word.toCharArray()) {
+                int idx = ch - 'a';
+                if (node.children[idx] == null) {
+                    node.children[idx] = new TrieNode();
+                }
+                node = node.children[idx];
+                node.prefixCount++;
+            }
+            node.wordCount++;
+        }
+        
+        boolean solve(String word , TrieNode node , int i){
+            if(i >= word.length()){
+                if(node.wordCount > 0)
+                return true;
+                else
+                return false;
+            }
+            boolean ans = false;
+            for(int j = 0; j < 26; j++){
+                if(word.charAt(i) != '.'){
+                    if(node.children[j] != null && j == (int)(word.charAt(i)-'a'))
+                    ans = ans||solve(word , node.children[j] , i+1);
+                }else{
+                    if(node.children[j] != null)
+                    ans = ans||solve(word , node.children[j] , i+1);
+                }
+            }
+            return ans;
+        }
+        public boolean search(String word) {
+            /*TrieNode node = root;
+            for (char ch : word.toCharArray()) {
+                int idx = ch - 'a';
+                if (node.children[idx] == null) return false;
+                node = node.children[idx];
+            }
+            return node.wordCount > 0;*/
+            TrieNode node = root;
+            return solve(word , node , 0);
+        }
+    }
+
+    Trie trie;
+    public WordDictionary() {
+        trie = new Trie();
+    }
+    
+    public void addWord(String word) {
+        trie.insert(word);
+    }
+    
+    public boolean search(String word) {
+        return trie.search(word);
+    }
+}
+
+/**
+ * Your WordDictionary object will be instantiated and called as such:
+ * WordDictionary obj = new WordDictionary();
+ * obj.addWord(word);
+ * boolean param_2 = obj.search(word);
+ */
