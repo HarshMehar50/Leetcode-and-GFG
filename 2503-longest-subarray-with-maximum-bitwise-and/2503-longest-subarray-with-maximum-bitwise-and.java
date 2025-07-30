bf9a1ccd932a9f2e73aa56a8f36112de86a3b705
HarshMehar50 <harshmehar4955@gmail.com>
2025-07-30 18:11:41 +0530

    Time: 49 ms (7.05%), Space: 62.1 MB (5.81%) - LeetHub

---
+++ 2503-longest-subarray-with-maximum-bitwise-and/2503-longest-subarray-with-maximum-bitwise-and.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int longestSubarray(int[] nums) {
+        int max = 0;
+        for(int i = 0; i < nums.length; i++){
+            max = Math.max(max , nums[i]);
+        }
+        TreeMap<Integer , Integer> f = new TreeMap<>();
+        int l = 0;
+        int ans = 0;
+        for(int r = 0; r < nums.length; r++){
+            f.put(nums[r] , f.getOrDefault(nums[r] , 0)+1);
+            while(l <= r && !(f.size() == 1 && f.firstKey() == max)){
+                f.put(nums[l] , f.get(nums[l])-1);
+                if(f.get(nums[l]) == 0)
+                f.remove(nums[l]);
+                l++;
+            }
+            if(f.size() == 1 && f.firstKey() == max)
+            ans = Math.max(ans , r-l+1);
+        } 
+        return ans;
+    }
+}
