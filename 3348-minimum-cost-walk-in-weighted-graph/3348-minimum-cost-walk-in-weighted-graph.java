089145a2bbc1b6f569c814fb19ebe6ee29722651
HarshMehar50 <harshmehar4955@gmail.com>
2025-08-05 08:26:59 +0530

    Time: 27 ms (16.81%), Space: 128.3 MB (46.02%) - LeetHub

--- 3348-minimum-cost-walk-in-weighted-graph/3348-minimum-cost-walk-in-weighted-graph.java
+++ 3348-minimum-cost-walk-in-weighted-graph/3348-minimum-cost-walk-in-weighted-graph.java
@@ -1,35 +1,81 @@
 class Solution {
-    int find(int x , int[] parent){
-        if(parent[x] == x)
-            return x;
-        return parent[x] = find(parent[x] , parent);
+    class DisjointSet{
+        int[] parent;
+        int[] rank;
+        int[] size;
+        public DisjointSet(int n){
+            parent = new int[n];
+            rank = new int[n];
+            size = new int[n];
+            for(int i = 0; i < n; i++){
+                parent[i] = i;
+                size[i] = 1;
+            }
+        }
+        int findParent(int node){
+            if(parent[node] == node){
+                return node;
+            }
+            return parent[node] = findParent(parent[node]);
+        }
+        void unionSetRank(int u, int v){
+            int up = findParent(u);
+            int vp = findParent(v);
+            if(up == vp)
+                return;
+            if(rank[up] < rank[vp]){
+                parent[up] = vp;
+            }else if(rank[vp] < rank[up]){
+                parent[vp] = up;
+            }else{
+                parent[up] = vp;
+                rank[vp]++;
+            }
+        }
+        void unionSetSize(int u , int v){
+            int up = findParent(u);
+            int vp = findParent(v);
+            if(up == vp)
+                return;
+            if(size[up] < size[vp]){
+                parent[up] = vp;
+                size[vp] = size[vp]+size[up];
+            }else{
+                parent[vp] = up;
+                size[up] = size[up]+size[vp];
+            }
+        }
     }
     public int[] minimumCost(int n, int[][] edges, int[][] query) {
-        int[] parent = new int[n];
-        int[] cost = new int[n];
-        Arrays.fill(cost , -1);
-        int[] ans = new int[query.length];
+        DisjointSet ds = new DisjointSet(n);
+        for(int i = 0; i < edges.length; i++){
+            ds.unionSetRank(edges[i][0] , edges[i][1]);
+        }
+        HashMap<Integer , List<Integer>> map = new HashMap<>();
         for(int i = 0; i < n; i++){
-            parent[i] = i;
+            map.put(ds.findParent(i) , new ArrayList<>());
         }
         for(int i = 0; i < edges.length; i++){
-            int pu = find(edges[i][0] , parent);
-            int pv = find(edges[i][1] , parent);
-            if(pu != pv){
-                cost[pu] = cost[pu]&cost[pv];
-                parent[pv] = pu;
-            }
-            cost[pu] = cost[pu]&edges[i][2];
+            map.get(ds.findParent(edges[i][0])).add(edges[i][2]);
         }
+        int[] cand = new int[n];
+        for(Integer x : map.keySet()){
+            if(map.get(x).size() > 0){
+            int compand = map.get(x).get(0);
+            for(Integer y : map.get(x)){
+                compand = compand&y;
+            }
+            cand[x] = compand;
+            }
+        }
+        int[] ans = new int[query.length];
         for(int i = 0; i < ans.length; i++){
-            int p1 = find(query[i][0] , parent);
-            int p2 = find(query[i][1] , parent);
             if(query[i][0] == query[i][1])
-                ans[i] = 0;
-            else if(p1 != p2)
-                ans[i] = -1;
+            ans[i] = 0;
+            else if(ds.findParent(query[i][0]) != ds.findParent(query[i][1]))
+            ans[i] = -1;
             else
-                ans[i] = cost[p1];
+            ans[i] = cand[ds.findParent(query[i][0])];
         }
         return ans;
     }
