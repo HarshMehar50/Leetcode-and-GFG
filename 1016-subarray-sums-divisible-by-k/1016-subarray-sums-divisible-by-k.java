eecd613b9e6e8783c7e66bc85007b6c703176f30
HarshMehar50 <harshmehar4955@gmail.com>
2025-08-12 21:55:28 +0530

    Time: 24 ms (50.65%), Space: 49.5 MB (20.42%) - LeetHub

--- 1016-subarray-sums-divisible-by-k/1016-subarray-sums-divisible-by-k.java
+++ 1016-subarray-sums-divisible-by-k/1016-subarray-sums-divisible-by-k.java
@@ -1,18 +1,26 @@
 class Solution {
     public int subarraysDivByK(int[] nums, int k) {
-        //int ps = 0;
-        int ans = 0;
-        int[] ps = new int[nums.length];
+        /*int[] ps = new int[nums.length];
         ps[0] = nums[0];
         for(int i = 1; i < nums.length; i++){
             ps[i] = ps[i-1]+nums[i];
         }
         HashMap<Integer , Integer> map = new HashMap<>();
         map.put(0 , 1);
-        for(int i = 0; i < nums.length; i++){
-            //ps += nums[i];
+        int ans = 0;
+        for(int i = 1; i < nums.length; i++){
             ans += map.getOrDefault(((ps[i]%k)+k)%k , 0);
             map.put(((ps[i]%k)+k)%k , map.getOrDefault(((ps[i]%k)+k)%k , 0)+1);
+        }
+        return ans;*/
+        int ps = 0;
+        HashMap<Integer , Integer> map = new HashMap<>();
+        map.put(0 , 1);
+        int ans = 0;
+        for(int i = 0; i < nums.length; i++){
+            ps += nums[i];
+            ans += map.getOrDefault(((ps%k)+k)%k , 0);
+            map.put(((ps%k)+k)%k , map.getOrDefault(((ps%k)+k)%k , 0)+1);
         }
         return ans;
     }
