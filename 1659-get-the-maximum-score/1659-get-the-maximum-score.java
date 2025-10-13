b44a340fb2ec3a9e425fe1dc59cde2e9fc330769
HarshMehar50 <harshmehar4955@gmail.com>
2025-10-14 00:37:29 +0530

    Time: 66 ms (5.3%), Space: 72.8 MB (5.3%) - LeetHub

--- 1659-get-the-maximum-score/1659-get-the-maximum-score.java
+++ 1659-get-the-maximum-score/1659-get-the-maximum-score.java
@@ -1,6 +1,6 @@
 class Solution {
     final int mod = 1000000007;
-    public long solve(int[] nums1 , int[] nums2 , HashMap<Integer , Integer> map1 , HashMap<Integer , Integer> map2 , int i , int g , long[][] dp){
+    long solve(int[] nums1 , int[] nums2 , int i , int g , long[][] dp){
         if(g == 1 && i >= nums1.length)
         return 0;
         if(g == 0 && i >= nums2.length)
@@ -9,32 +9,22 @@ class Solution {
         return dp[i][g];
         long same = 0;
         long change = 0;
-        long ans = 0;
         if(g == 1){
-            same = nums1[i]+solve(nums1 , nums2 , map1 , map2 , i+1 , 1 , dp);
-            if(map2.containsKey(nums1[i]))
-            change = nums1[i]+solve(nums1 , nums2 , map1 , map2 , map2.get(nums1[i])+1 , 0 , dp);
-            ans = Math.max(same , change);
-            dp[i][g] = ans;
-            return dp[i][g];
+            same = nums1[i]+solve(nums1 , nums2 , i+1 , 1 , dp);
+            int bs = Arrays.binarySearch(nums2 , nums1[i]);
+            if(bs >= 0 && bs < nums2.length)
+            change = nums1[i]+solve(nums1 , nums2 , bs+1 , 0 , dp);
         }else{
-            same = nums2[i]+solve(nums1 , nums2 , map1 , map2 , i+1 , 0 , dp);
-            if(map1.containsKey(nums2[i]))
-            change = nums2[i]+solve(nums1 , nums2 , map1 , map2 , map1.get(nums2[i])+1 , 1 , dp);
-            ans = Math.max(same , change);
-            dp[i][g] = ans;
-            return dp[i][g];
+            same = nums2[i]+solve(nums1 , nums2 , i+1 , 0 , dp);
+            int bs = Arrays.binarySearch(nums1 , nums2[i]);
+            if(bs >= 0 && bs < nums1.length)
+            change = nums2[i]+solve(nums1 , nums2 , bs+1 , 1 , dp);
         }
+        long ans = Math.max(same , change);
+        dp[i][g] = ans;
+        return dp[i][g];
     }
     public int maxSum(int[] nums1, int[] nums2) {
-        HashMap<Integer , Integer> map1 = new HashMap<>();
-        HashMap<Integer , Integer> map2 = new HashMap<>();
-        for(int i = 0; i < nums1.length; i++){
-            map1.put(nums1[i] , i);
-        }
-        for(int i = 0; i < nums2.length; i++){
-            map2.put(nums2[i] , i);
-        }
         long[][] dp1 = new long[Math.max(nums1.length , nums2.length)][2];
         long[][] dp2 = new long[Math.max(nums1.length , nums2.length)][2];
         for(long[] x : dp1){
@@ -43,6 +33,6 @@ class Solution {
         for(long[] x : dp2){
             Arrays.fill(x , -1);
         }
-        return (int)Math.max(solve(nums1 , nums2 , map1 , map2 , 0 , 1 , dp1)%mod , solve(nums1 , nums2 , map1 , map2 , 0 , 0 , dp2)%mod);
+        return (int)Math.max(solve(nums1 , nums2 , 0 , 1 , dp1)%mod , solve(nums1 , nums2 , 0 , 0 , dp2)%mod);
     }
 }
