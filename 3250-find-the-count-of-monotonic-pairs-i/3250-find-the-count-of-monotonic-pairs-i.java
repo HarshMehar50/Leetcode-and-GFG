7552b6ff6fd4b31c91450ced088758ce9f0ec68d
HarshMehar50 <harshmehar4955@gmail.com>
2025-12-30 19:19:31 +0530

    Time: 99 ms (41.94%), Space: 47 MB (87.1%) - LeetHub

--- 3250-find-the-count-of-monotonic-pairs-i/3250-find-the-count-of-monotonic-pairs-i.java
+++ 3250-find-the-count-of-monotonic-pairs-i/3250-find-the-count-of-monotonic-pairs-i.java
@@ -1,31 +1,35 @@
 class Solution {
     final int mod = 1000000007;
-    int solve(int[] nums , int i , int p1 , int p2 , int[][][] dp){
+    int solve(int[] nums , int i , int p , int[][] dp){
         if(i >= nums.length)
         return 1;
-        if(dp[i][p1+1][p2+1] != -1)
-        return dp[i][p1+1][p2+1];
+        if(dp[i][p+1] != -1)
+        return dp[i][p+1];
         int ans = 0;
-        if(p1 == -1 && p2 == -1){
+        if(p == -1){
             for(int j = 0; j <= nums[i]; j++){
-                ans = (ans+solve(nums , i+1 , j , nums[i]-j , dp))%mod;
+                ans = (ans+solve(nums , i+1 , j , dp))%mod;
             }
         }else{
+            int ps = nums[i-1];
+            int p2 = ps-p;
             for(int j = 0; j <= nums[i]; j++){
-                if(j >= p1 && nums[i]-j <= p2)
-                ans = (ans+solve(nums , i+1 , j , nums[i]-j , dp))%mod;
+                if(j >= p && nums[i]-j <= p2)
+                ans = (ans+solve(nums , i+1 , j , dp))%mod;
             }
         }
-        dp[i][p1+1][p2+1] = ans;
-        return dp[i][p1+1][p2+1];
+        dp[i][p+1] = ans;
+        return dp[i][p+1];
     }
     public int countOfPairs(int[] nums) {
-        int[][][] dp = new int[nums.length][52][52];
-        for(int[][] a : dp){
-            for(int[] b : a){
-                Arrays.fill(b , -1);
-            }
+        int max = 0;
+        for(int i = 0; i < nums.length; i++){
+            max = Math.max(max , nums[i]);
         }
-        return solve(nums , 0 , -1 , -1 , dp);
+        int[][] dp = new int[nums.length][max+2];
+        for(int[] a : dp){
+            Arrays.fill(a , -1);
+        }
+        return solve(nums , 0 , -1 , dp);
     }
 }
