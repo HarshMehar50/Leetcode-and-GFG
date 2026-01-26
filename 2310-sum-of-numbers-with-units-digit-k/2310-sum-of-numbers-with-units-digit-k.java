08ebee25ee7fb5ec34d06880f3de2077620f9cac
HarshMehar50 <harshmehar4955@gmail.com>
2026-01-26 17:04:46 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2310-sum-of-numbers-with-units-digit-k/2310-sum-of-numbers-with-units-digit-k.java
+++ 2310-sum-of-numbers-with-units-digit-k/2310-sum-of-numbers-with-units-digit-k.java
@@ -1,22 +1,19 @@
 class Solution {
-    int solve(int num , int k , int s , int[][] dp){
+    int solve(int num , int k , int s , HashMap<Pair<Integer , Integer> , Integer> dp){
         if(num == 0)
         return s;
-        if(dp[num][s] != -1)
-        return dp[num][s];
+        if(dp.containsKey(new Pair<>(num , s)))
+        return dp.get(new Pair<>(num , s));
         int ans = Integer.MAX_VALUE;
         for(int i = k; i <= num; i += 10){
             if(i != 0)
             ans = Math.min(ans , solve(num-i , k , s+1 , dp));
         }
-        dp[num][s] = ans;
-        return dp[num][s];
+        dp.put(new Pair<>(num , s) , ans);
+        return dp.get(new Pair<>(num , s));
     }
     public int minimumNumbers(int num, int k) {
-        int[][] dp = new int[num+1][3001];
-        for(int[] a : dp){
-            Arrays.fill(a , -1);
-        }
+        HashMap<Pair<Integer , Integer> , Integer> dp = new HashMap<>();
         int ans = solve(num , k , 0 , dp);
         if(ans == Integer.MAX_VALUE)
         return -1;
