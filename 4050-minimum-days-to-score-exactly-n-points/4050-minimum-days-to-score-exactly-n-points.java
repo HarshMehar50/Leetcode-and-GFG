521c234207823233d2a374f048de39b87c30509c
HarshMehar50 <harshmehar4955@gmail.com>
2026-09-26 00:00:26 +0530

    Time: 358 ms (19.26%), Space: 60.9 MB (12.55%) - LeetHub

---
+++ 4050-minimum-days-to-score-exactly-n-points/4050-minimum-days-to-score-exactly-n-points.java
@@ -0,0 +1,22 @@
+class Solution {
+    int solve(int n , int[] dp){
+        if(n == 0)
+        return 0;
+        if(dp[n] != -1)
+        return dp[n];
+        int ans = Integer.MAX_VALUE;
+        for(int i = 1; i*(i+1)/2 <= n; i++){
+            int c = i;
+            if(n-(i*(i+1)/2) != 0)
+            c++;
+            ans = Math.min(ans , c+solve(n-(i*(i+1)/2) , dp));
+        }
+        dp[n] = ans;
+        return dp[n];
+    }
+    public int minDays(int n) {
+        int[] dp = new int[n+1];
+        Arrays.fill(dp , -1);
+        return solve(n , dp);
+    }
+}
